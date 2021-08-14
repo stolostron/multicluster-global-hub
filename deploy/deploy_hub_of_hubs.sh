d242c1c8b0f73d0bf0862bfb68901882666a066f
Vadim Eisenberg <vadime@il.ibm.com>
2021-08-14 13:48:11 +0300

    update custom repos

--- deploy/deploy_hub_of_hubs.sh
+++ deploy/deploy_hub_of_hubs.sh
@@ -11,6 +11,9 @@ sync_service_port=9689
 
 echo "using kubeconfig $KUBECONFIG"
 
+kubectl create configmap custom-repos --from-file=hub_of_hubs_custom_repos.json -n "$acm_namespace"
+kubectl annotate mch multiclusterhub  --overwrite mch-imageOverridesCM=custom-repos  -n "$acm_namespace"
+
 # apply the HoH config CRD
 kubectl apply -f https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-crds/main/crds/hub-of-hubs.open-cluster-management.io_config_crd.yaml
 
