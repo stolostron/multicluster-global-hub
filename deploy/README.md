4f939d158887bcb4583892e3b0433fdf40e5a38c
Vadim Eisenberg <vadime@il.ibm.com>
2021-08-14 12:16:59 +0300

    initial version of the PoC deployment scripts

---
+++ deploy/README.md
@@ -0,0 +1,38 @@
+# Deployment instructions for Hub-of-Hubs
+
+1.  Define the `KUBECONFIG` variable to hold the kubernetes configuration of Hub-of-Hubs
+
+1.  Define the REGISTRY environment variable, for example:
+
+    ```
+    export REGISTRY=docker.io/<your user name>
+    ```
+
+1.  Define the release tag variable for images:
+
+    ```
+    export IMAGE_TAG=v0.1.0
+    ```
+
+1.  Define the `DATABASE_URL_HOH` and `DATABASE_URL_TRANSPORT` variables for the HoH and Transport users
+
+Set the `DATABASE_URL` according to the PostgreSQL URL format: `postgres://YourUserName:YourURLEscapedPassword@YourHostname:5432/YourDatabaseName?sslmode=verify-full`.
+
+:exclamation: Remember to URL-escape the password, you can do it in bash:
+
+```
+python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])" 'YourPassword'
+```
+
+## Linting
+
+**Prerequisite**: install the `shellcheck` tool (a Linter for shell):
+
+```
+brew install shellcheck
+```
+
+Run
+```
+shellcheck *.sh
+```
---
+++ deploy/deploy_hub_of_hubs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright (c) 2021 Red Hat, Inc.
+# Copyright Contributors to the Open Cluster Management project
+
+set -o errexit
+set -o nounset
+
+acm_namespace=open-cluster-management
+
+echo "using kubeconfig $KUBECONFIG"
+
+# apply the HoH config CRD
+kubectl apply -f https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-crds/main/crds/hub-of-hubs.open-cluster-management.io_config_crd.yaml
+
+kubectl delete secret hub-of-hubs-database-secret -n "$acm_namespace" --ignore-not-found
+kubectl create secret generic hub-of-hubs-database-secret -n "$acm_namespace" --from-literal=url="$DATABASE_URL_HOH"
+curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-spec-sync/$TAG/deploy/operator.yaml.template" | IMAGE_TAG=$TAG COMPONENT=hub-of-hubs-spec-sync envsubst | kubectl apply -f - -n "$acm_namespace"
+curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-status-sync/$TAG/deploy/operator.yaml.template" | IMAGE_TAG=$TAG COMPONENT=hub-of-hubs-status-sync envsubst | kubectl apply -f - -n "$acm_namespace"
