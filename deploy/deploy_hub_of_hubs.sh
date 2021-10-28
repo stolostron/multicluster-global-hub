bd893e2c61f058946b9a5a3341d3fd9f1363b937
Ian Zhang <izhang@redhat.com>
2021-10-28 16:30:44 -0400

    fix for comments
    Signed-off-by: Ian Zhang <izhang@redhat.com>

--- deploy/deploy_hub_of_hubs.sh
+++ deploy/deploy_hub_of_hubs.sh
@@ -8,26 +8,19 @@ set -o nounset
 
 TAG=${TAG:="v0.1.0"}
 
-PG_NAMESPACE="hoh-postgres"
-PGO_PREFIX="hoh-pguser-"
-PROCESS_USER="${PGO_PREFIX}hoh-process-user"
-TRANSPORT_USER="${PGO_PREFIX}transport-bridge-user"
-HOH_USER="${PGO_PREFIX}hoh"
+pg_namespace="hoh-postgres"
+pgo_prefix="hoh-pguser-"
+process_user="${pgo_prefix}hoh-process-user"
+transport_user="${pgo_prefix}transport-bridge-user"
 
-echo "hoh user:" "$(kubectl get secrets -n "${PG_NAMESPACE}" "${HOH_USER}" -o go-template='{{.data.password | base64decode}}')"
-#echo "$(kubectl get secrets -n "${PG_NAMESPACE}" "${PROCESS_USER}" -o go-template='{{.data.password | base64decode}}')"
-#echo "$(kubectl get secrets -n "${PG_NAMESPACE}" "${TRANSPORT_USER}" -o go-template='{{.data.password | base64decode}}')"
+pg_process_user_URI="$(kubectl get secrets -n "${pg_namespace}" "${process_user}" -o go-template='{{index (.data) "pgbouncer-uri" | base64decode}}')"
+pg_transport_user_URI="$(kubectl get secrets -n "${pg_namespace}" "${transport_user}" -o go-template='{{index (.data) "pgbouncer-uri" | base64decode}}')"
 
-PG_PROCESS_USER_URI="$(kubectl get secrets -n "${PG_NAMESPACE}" "${PROCESS_USER}" -o go-template='{{.data.uri | base64decode}}')"
-PG_TRANSPORT_USER_URI="$(kubectl get secrets -n "${PG_NAMESPACE}" "${TRANSPORT_USER}" -o go-template='{{.data.uri | base64decode}}')"
-
-PROCESSOR_URI=${DATABASE_URL_HOH:=$PG_PROCESS_USER_URI}
-TRANSPORT_URI=${DATABASE_URL_TRANSPORT:=$PG_TRANSPORT_USER_URI}
-
-echo $PROCESSOR_URI
+database_url_hoh=${DATABASE_URL_HOH:=$pg_process_user_URI}
+database_url_transport=${DATABASE_URL_TRANSPORT:=$pg_transport_user_URI}
 
 acm_namespace=open-cluster-management
-css_sync_service_host=${CSS_SYNC_SERVICE_HOST:-ec2-107-21-78-35.compute-1.amazonaws.com}
+css_sync_service_host=${CSS_SYNC_SERVICE_HOST:-my-postgres.com}
 css_sync_service_port=${CSS_SYNC_SERVICE_PORT:-9689}
 
 echo "using kubeconfig $KUBECONFIG"
@@ -46,14 +39,14 @@ kubectl create namespace hoh-system --dry-run -o yaml | kubectl apply -f -
 kubectl apply -f "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-crds/$TAG/cr-examples/hub-of-hubs.open-cluster-management.io_config_cr.yaml" -n hoh-system
 
 kubectl delete secret hub-of-hubs-database-secret -n "$acm_namespace" --ignore-not-found
-kubectl create secret generic hub-of-hubs-database-secret -n "$acm_namespace" --from-literal=url="$PROCESSOR_URI"
+kubectl create secret generic hub-of-hubs-database-secret -n "$acm_namespace" --from-literal=url="$database_url_hoh"
 curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-spec-sync/$TAG/deploy/operator.yaml.template" |
     REGISTRY=vadimeisenbergibm IMAGE_TAG=$TAG COMPONENT=hub-of-hubs-spec-sync envsubst | kubectl apply -f - -n "$acm_namespace"
 curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-status-sync/$TAG/deploy/operator.yaml.template" |
     REGISTRY=vadimeisenbergibm IMAGE_TAG=$TAG COMPONENT=hub-of-hubs-status-sync envsubst | kubectl apply -f - -n "$acm_namespace"
 
 kubectl delete secret hub-of-hubs-database-transport-bridge-secret -n "$acm_namespace" --ignore-not-found
-kubectl create secret generic hub-of-hubs-database-transport-bridge-secret -n "$acm_namespace" --from-literal=url="$TRANSPORT_URI"
+kubectl create secret generic hub-of-hubs-database-transport-bridge-secret -n "$acm_namespace" --from-literal=url="$database_url_transport"
 
 curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-spec-transport-bridge/$TAG/deploy/hub-of-hubs-spec-transport-bridge.yaml.template" |
     SYNC_SERVICE_HOST="$css_sync_service_host" SYNC_SERVICE_PORT="$css_sync_service_port" IMAGE="nirrozenbaumibm/hub-of-hubs-spec-transport-bridge:$TAG" envsubst | kubectl apply -f - -n "$acm_namespace"
--- deploy/undeploy_hub_of_hubs.sh
+++ deploy/undeploy_hub_of_hubs.sh
@@ -9,25 +9,26 @@ set -o nounset
 acm_namespace=open-cluster-management
 
 echo "using kubeconfig $KUBECONFIG"
-kubectl delete namespace hoh-system --ignore-not-found
 
-curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-spec-sync/$TAG/deploy/operator.yaml.template" | \
-REGISTRY=vadimeisenbergibm IMAGE_TAG="$TAG" COMPONENT=hub-of-hubs-spec-sync envsubst | kubectl delete -f - -n "$acm_namespace" --ignore-not-found
-curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-status-sync/$TAG/deploy/operator.yaml.template" | \
-REGISTRY=vadimeisenbergibm IMAGE_TAG="$TAG" COMPONENT=hub-of-hubs-status-sync envsubst | kubectl delete -f - -n "$acm_namespace" --ignore-not-found
+curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-spec-sync/$TAG/deploy/operator.yaml.template" |
+    REGISTRY=vadimeisenbergibm IMAGE_TAG="$TAG" COMPONENT=hub-of-hubs-spec-sync envsubst | kubectl delete -f - -n "$acm_namespace" --ignore-not-found
+curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-status-sync/$TAG/deploy/operator.yaml.template" |
+    REGISTRY=vadimeisenbergibm IMAGE_TAG="$TAG" COMPONENT=hub-of-hubs-status-sync envsubst | kubectl delete -f - -n "$acm_namespace" --ignore-not-found
 
 kubectl delete secret hub-of-hubs-database-secret -n "$acm_namespace" --ignore-not-found
 
-curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-spec-transport-bridge/$TAG/deploy/hub-of-hubs-spec-transport-bridge.yaml.template" | \
-SYNC_SERVICE_PORT="" IMAGE="" envsubst | kubectl delete -f - -n "$acm_namespace" --ignore-not-found
-curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-status-transport-bridge/$TAG/deploy/hub-of-hubs-status-transport-bridge.yaml.template" | \
+curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-spec-transport-bridge/$TAG/deploy/hub-of-hubs-spec-transport-bridge.yaml.template" |
+    SYNC_SERVICE_PORT="" IMAGE="" envsubst | kubectl delete -f - -n "$acm_namespace" --ignore-not-found
+curl -s "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-status-transport-bridge/$TAG/deploy/hub-of-hubs-status-transport-bridge.yaml.template" |
     SYNC_SERVICE_PORT="" IMAGE="" envsubst | kubectl delete -f - -n "$acm_namespace" --ignore-not-found
 
 kubectl delete secret hub-of-hubs-database-secret-transport-bridge-secret -n "$acm_namespace" --ignore-not-found
 
 # delete the HoH config CRD
 kubectl delete -f "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-crds/$TAG/crds/hub-of-hubs.open-cluster-management.io_config_crd.yaml" \
-	--ignore-not-found
+    --ignore-not-found
 
-kubectl annotate mch multiclusterhub  --overwrite mch-imageOverridesCM=  -n "$acm_namespace"
+kubectl annotate mch multiclusterhub --overwrite mch-imageOverridesCM= -n "$acm_namespace"
 kubectl delete configmap custom-repos -n "$acm_namespace" --ignore-not-found
+
+kubectl delete namespace hoh-system --ignore-not-found
