a99ec7af12a2c4277a6e63beb46070a09dce1526
Maroon Ayoub <maroon.ayoub@ibm.com>
2021-12-27 10:36:13 +0200

    undeploy leaf-hub syncers before deleting ns (#17)
    * undeploy leaf-hub syncers before deleting ns
    
    not scaling down controllers has been found to sometimes skip graceful shutdowns, causing finalizers placed by the controllers not getting removed.
    - changed order of deletion: syncers before hoh-system namespace (policies are in hot-system namespace with a finalizer from leaf-hub-status-sync)
    - descale syncers to 0 before deleting deployments
    
    * removed descale
    
    removed descale, delete alone is sufficient if terminationGracePeriodSeconds is set in deployment spec

--- deploy/undeploy_leaf_hub.sh
+++ deploy/undeploy_leaf_hub.sh
@@ -9,12 +9,13 @@ set -o nounset
 acm_namespace=open-cluster-management
 
 echo "using kubeconfig $KUBECONFIG"
-kubectl delete namespace hoh-system --ignore-not-found
-
+# delete deployments before namespace (to remove finalizers first)
 curl -s "https://raw.githubusercontent.com/open-cluster-management/leaf-hub-spec-sync/$TAG/deploy/leaf-hub-spec-sync.yaml.template" | \
     envsubst | kubectl delete -f - --ignore-not-found
 curl -s "https://raw.githubusercontent.com/open-cluster-management/leaf-hub-status-sync/$TAG/deploy/leaf-hub-status-sync.yaml.template" | \
     envsubst | kubectl delete -f - --ignore-not-found
+    
+kubectl delete namespace hoh-system --ignore-not-found
 
 # delete the HoH config CRD
 kubectl delete -f "https://raw.githubusercontent.com/open-cluster-management/hub-of-hubs-crds/$TAG/crds/hub-of-hubs.open-cluster-management.io_config_crd.yaml" \
