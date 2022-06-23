67bf3efc746e28534c2fc7acbb9778c9eacaf1f7
Meng <yanmxa@163.com>
2022-06-23 10:43:23 +0800

    install postgres with local resource
    Signed-off-by: Meng <yanmxa@163.com>

--- test/setup/postgres/postgres_setup.sh
+++ test/setup/postgres/postgres_setup.sh
@@ -1,38 +1,56 @@
+#!/bin/bash
+# Source this script to enable the postgres in hub-of-hubs. DATABASE_URL_HOH and DATABASE_URL_HOH could be used to init hub-of-hubs
 
-# source this script to enable the postgres in hub-of-hubs. DATABASE_URL_HOH and DATABASE_URL_HOH could be used to init hub-of-hubs
+function installPostgres() {
+  currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  namespace="hoh-postgres"
+  userSecrets=("hoh-pguser-hoh-process-user" "hoh-pguser-postgres" "hoh-pguser-transport-bridge-user")
 
-function deployComponent() {
-  # deploy component
-  # before: checkout code
-  rm -rf $1
-  git clone https://github.com/stolostron/$1.git
-  cd $1
-  git checkout $2
-  # action
-  $3
-  # after
-  cd ..
-  rm -rf $1
+  # ensure the pgo operator crd and other stuff is deleted first to start its deployment from scratch
+  kubectl delete -k ${currentDir}/install --ignore-not-found=true 2>/dev/null
+
+  # ensure all the user secrets are deleted
+  for secret in ${userSecrets[@]}
+  do
+    matched=$(kubectl get secret $secret -n $namespace --ignore-not-found=true)
+    while [ ! -z "$matched" ]; do
+      echo "Waiting for secret $secret to be deleted from namespace $namespace"
+      matched=$(kubectl get secret $secret -n $namespace --ignore-not-found=true)
+      sleep 10
+    done
+  done
+
+  # install the pgo operator to postgres-operator
+  kubectl apply -k ${currentDir}/install
+
+  # ensure all the user secrets are created
+  for secret in ${userSecrets[@]}
+  do
+    matched=$(kubectl get secret $secret -n $namespace --ignore-not-found=true)
+    while [ -z "$matched" ]; do
+      echo "Waiting for secret $secret to be created in namespace $namespace"
+      matched=$(kubectl get secret $secret -n $namespace --ignore-not-found=true)
+      sleep 10
+    done
+  done
+
+  kubectl delete -f ${currentDir}/postgres-job.yaml --ignore-not-found=true
+
+  IMAGE=quay.io/open-cluster-management-hub-of-hubs/postgresql-ansible:$TAG 
+  envsubst < ${currentDir}/postgres-job.yaml | kubectl apply -f -
+
+  kubectl wait --for=condition=complete job/postgres-init -n $namespace --timeout=300s
+  kubectl logs $(kubectl get pods --field-selector status.phase=Succeeded  --selector=job-name=postgres-init -n $namespace  --output=jsonpath='{.items[*].metadata.name}') -n $namespace
 }
 
-function deployHohPostgresqlAction() {
-  cd ./pgo
-  IMAGE=quay.io/open-cluster-management-hub-of-hubs/postgresql-ansible:$TAG ./setup.sh
-  cd ..
-}
+# always check whether DATABASE_URL_HOH and DATABASE_URL_TRANSPORT are set, if not - install PGO and use its secrets
+if [ -z "${DATABASE_URL_HOH-}" ] && [ -z "${DATABASE_URL_TRANSPORT-}" ]; then
+  installPostgres
 
-function deployPostgres() {
-  # always check whether DATABASE_URL_HOH and DATABASE_URL_TRANSPORT are set, if not - install PGO and use its secrets
-  if [ -z "${DATABASE_URL_HOH-}" ] && [ -z "${DATABASE_URL_TRANSPORT-}" ]; then
-    deployComponent "hub-of-hubs-postgresql" "$branch" deployHohPostgresqlAction
+  namespace="hoh-postgres"
+  processUser="hoh-pguser-hoh-process-user"
+  transportUser="hoh-pguser-transport-bridge-user"
 
-    pg_namespace="hoh-postgres"
-    process_user="hoh-pguser-hoh-process-user"
-    transport_user="hoh-pguser-transport-bridge-user"
-
-    DATABASE_URL_HOH="$(kubectl get secrets -n "${pg_namespace}" "${process_user}" -o go-template='{{index (.data) "pgbouncer-uri" | base64decode}}')"
-    DATABASE_URL_HOH="$(kubectl get secrets -n "${pg_namespace}" "${transport_user}" -o go-template='{{index (.data) "pgbouncer-uri" | base64decode}}')"
-  fi
-}
-
-deployPostgres
+  DATABASE_URL_HOH="$(kubectl get secrets -n "${namespace}" "${processUser}" -o go-template='{{index (.data) "pgbouncer-uri" | base64decode}}')"
+  DATABASE_URL_TRANSPORT="$(kubectl get secrets -n "${namespace}" "${transportUser}" -o go-template='{{index (.data) "pgbouncer-uri" | base64decode}}')"
+fi
