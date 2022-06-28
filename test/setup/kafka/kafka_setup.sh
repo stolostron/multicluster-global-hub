fb0daa2386253eab5d5498d2e563cc65e9f3b8c7
myan <myan@redhat.com>
2022-06-28 12:55:07 +0800

    expose kafka with nodeport
    Signed-off-by: myan <myan@redhat.com>

--- test/setup/kafka/kafka_setup.sh
+++ test/setup/kafka/kafka_setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # Source this script to enable the kafka cluster for hub-of-hubs. The olm component must be install before exec this script.
 
-KAFKA_OPERATOR=${KAFKA_OPERATOR:-"strimzi-cluster-operator-v0.23.0"}
-
 function deployKafka() {
+  clusterIsReady=$(kubectl -n kafka get kafka.kafka.strimzi.io/kafka-brokers-cluster -o jsonpath={.status.listeners} --ignore-not-found)
+  if [[ ! -z "$clusterIsReady" ]]; then 
+    echo "Kafka cluster already exists!"
+    exit 0
+  fi 
+
   # create namespace if not exists
   kubectl create namespace kafka --dry-run=client -o yaml | kubectl apply -f -
 
   # install community kafka operator
+  KAFKA_OPERATOR=${KAFKA_OPERATOR:-"strimzi-cluster-operator-v0.23.0"}
   currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   kubectl apply -f ${currentDir}/kafka-operator.yaml
     
@@ -15,7 +20,7 @@ function deployKafka() {
   operatorDeployed=$(kubectl -n kafka get Deployment/$KAFKA_OPERATOR --ignore-not-found)
   while [ -z "$operatorDeployed" ]; do
       echo "Waiting for strimzi-cluster-operator to become available"
-      sleep 10
+      sleep 20
       operatorDeployed=$(kubectl -n kafka get Deployment/$KAFKA_OPERATOR --ignore-not-found)
   done
   kubectl -n kafka wait --for=condition=Available Deployment/$KAFKA_OPERATOR --timeout=600s
