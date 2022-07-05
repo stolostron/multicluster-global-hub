af32404fea63622fbccfc8d6dfeeb716b2d2d8e5
Meng Yan <myan@redhat.com>
2022-07-05 14:18:44 +0800

    Modify the labels of the managed clusters with nonk8s api (#70)
    * modify the labels of the managed clusters
    
    Signed-off-by: myan <myan@redhat.com>
    
    * add AfterEach action for the label spec
    
    Signed-off-by: myan <myan@redhat.com>

--- test/pkg/test/hoh-label_test.go
+++ test/pkg/test/hoh-label_test.go
@@ -12,17 +12,18 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/stolostron/hub-of-hubs/test/pkg/utils"
 	"k8s.io/klog"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
+
+	"github.com/stolostron/hub-of-hubs/test/pkg/utils"
 )
 
 const (
-	CLUSTER_LABEL_KEY   = "cluster"
-	CLUSTER_LABEL_VALUE = "test"
+	CLUSTER_LABEL_KEY   = "test"
+	CLUSTER_LABEL_VALUE = "label"
 )
 
-var _ = Describe("label", Label("label"), Ordered, func() {
+var _ = Describe("Updating cluster label from HoH manager", Label("label"), Ordered, func() {
 	var token string
 	var httpClient *http.Client
 	var managedClusterName string
@@ -42,8 +43,15 @@ var _ = Describe("label", Label("label"), Ordered, func() {
 
 		By("Get a managed cluster name")
 		managedClusters := getManagedCluster(httpClient, token)
+		Expect(len(managedClusters)).Should(BeNumerically(">", 0), "should get the managed cluster")
 		managedClusterName = managedClusters[0].Name
-		klog.V(6).Info(fmt.Sprintf("get cluster: %s for the label test", managedClusterName))
+		Expect(len(managedClusterName)).Should(BeNumerically(">", 0))
+	})
+
+	AfterEach(func() {
+		By("Print result after updating the cluster label")
+		managedClusters := getManagedCluster(httpClient, token)
+		printClusterLabel(managedClusters)
 	})
 
 	It("add the label to the managed cluster", func() {
@@ -92,6 +100,7 @@ var _ = Describe("label", Label("label"), Ordered, func() {
 			}
 			return nil
 		}, 60*time.Second*5, 1*time.Second*5).ShouldNot(HaveOccurred())
+
 	})
 })
 
@@ -114,11 +123,11 @@ func getLeafHubName(managedClusterName string) string {
 }
 
 func getManagedCluster(client *http.Client, token string) []clusterv1.ManagedCluster {
-	managedClusterUrl := fmt.Sprintf("https://multicloud-console.apps.%s./multicloud/hub-of-hubs-nonk8s-api/managedclusters", testOptions.HubCluster.BaseDomain)
+	managedClusterUrl := fmt.Sprintf("%s/multicloud/hub-of-hubs-nonk8s-api/managedclusters", testOptions.HubCluster.Nonk8sApiServer)
 	req, err := http.NewRequest("GET", managedClusterUrl, nil)
 	Expect(err).ShouldNot(HaveOccurred())
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-
+	
 	By("Get response of the api")
 	resp, err := client.Do(req)
 	Expect(err).ShouldNot(HaveOccurred())
@@ -128,16 +137,16 @@ func getManagedCluster(client *http.Client, token string) []clusterv1.ManagedClu
 	body, err := ioutil.ReadAll(resp.Body)
 	Expect(err).ShouldNot(HaveOccurred())
 
+	By("Return parsed managedcluster")
 	var managedClusters []clusterv1.ManagedCluster
 	json.Unmarshal(body, &managedClusters)
-	Expect(len(managedClusters)).Should(BeNumerically(">", 1), "should get the managed cluster")
+	Expect(len(managedClusters)).Should(BeNumerically(">", 0), "should get the managed cluster")
 
-	By("Return parsed managedcluster")
 	return managedClusters
 }
 
 func updateClusterLabel(client *http.Client, patches []patch, token, managedClusterName string) {
-	updateLabelUrl := fmt.Sprintf("https://multicloud-console.apps.%s./multicloud/hub-of-hubs-nonk8s-api/managedclusters/%s", testOptions.HubCluster.BaseDomain, managedClusterName)
+	updateLabelUrl := fmt.Sprintf("%s/multicloud/hub-of-hubs-nonk8s-api/managedclusters/%s", testOptions.HubCluster.Nonk8sApiServer, managedClusterName)
 	// set method and body
 	jsonBody, err := json.Marshal(patches)
 	Expect(err).ShouldNot(HaveOccurred())
@@ -157,4 +166,12 @@ func updateClusterLabel(client *http.Client, patches []patch, token, managedClus
 	response, err := client.Do(req)
 	Expect(err).ShouldNot(HaveOccurred())
 	defer response.Body.Close()
+}
+
+func printClusterLabel(clusters []clusterv1.ManagedCluster) {
+	for _, cluster := range clusters {
+		for k, v := range cluster.GetObjectMeta().GetLabels() {
+			klog.V(5).Info(fmt.Sprintf("Cluster(%s): %s -> %s", cluster.Name, k, v))
+		}
+	}
 }
