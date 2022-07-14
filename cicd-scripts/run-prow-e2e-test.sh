11a0dfc95329022c588e1e5e5334b7b5e3e8ef16
myan <myan@redhat.com>
2022-07-14 21:58:04 +0800

    install clusteradm from binary
    Signed-off-by: myan <myan@redhat.com>

--- cicd-scripts/run-prow-e2e-test.sh
+++ cicd-scripts/run-prow-e2e-test.sh
@@ -22,6 +22,14 @@ ssh "${OPT[@]}" "$HOST" sudo wget https://dl.google.com/go/go1.17.7.linux-amd64.
 ssh "${OPT[@]}" "$HOST" sudo tar -C /usr/local/ -xvf go1.17.7.linux-amd64.tar.gz 
 echo "go version: $(go version)"
 
+ssh "${OPT[@]}" "$HOST" echo "PATH=$PATH"
+ssh "${OPT[@]}" "$HOST" sudo wget https://github.com/open-cluster-management-io/clusteradm/releases/download/v0.2.0/clusteradm_linux_amd64.tar.gz
+ssh "${OPT[@]}" "$HOST" sudo tar -C /usr/local/bin -xvf clusteradm_linux_amd64.tar.gz
+ssh "${OPT[@]}" "$HOST" export "PATH=${PATH}:/usr/local/bin"
+ssh "${OPT[@]}" "$HOST" echo "PATH=$PATH"
+
+echo "clusteradm help: $(clusteradm help)" 
+
 scp "${OPT[@]}" -r ../hub-of-hubs "$HOST:$HOST_DIR"
 ssh "${OPT[@]}" "$HOST" "cd $HOST_DIR && . test/resources/env.list && make e2e-setup-start && make e2e-tests-all"
 
