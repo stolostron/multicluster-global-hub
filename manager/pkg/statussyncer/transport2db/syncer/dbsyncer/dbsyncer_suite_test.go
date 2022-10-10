47a4620c30cd8ab8e54e4d3438e3c0f48fa27207
myan <myan@redhat.com>
2022-10-10 09:52:21 +0000

    add delta and minimal policy syncer test
    Signed-off-by: myan <myan@redhat.com>

--- manager/pkg/statussyncer/transport2db/syncer/dbsyncer/dbsyncer_suite_test.go
+++ manager/pkg/statussyncer/transport2db/syncer/dbsyncer/dbsyncer_suite_test.go
@@ -2,6 +2,7 @@ package dbsyncer_test
 
 import (
 	"context"
+	"encoding/json"
 	"os"
 	"path/filepath"
 	"testing"
@@ -20,6 +21,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	producer "github.com/stolostron/multicluster-global-hub/agent/pkg/transport/producer"
 	managerscheme "github.com/stolostron/multicluster-global-hub/manager/pkg/scheme"
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/specsyncer/db2transport/db/postgresql"
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/statistics"
@@ -27,7 +29,9 @@ import (
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/statussyncer/transport2db/db/workerpool"
 	statussyncer "github.com/stolostron/multicluster-global-hub/manager/pkg/statussyncer/transport2db/syncer"
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/statussyncer/transport2db/transport"
+	"github.com/stolostron/multicluster-global-hub/pkg/compressor"
 	"github.com/stolostron/multicluster-global-hub/pkg/constants"
+	"github.com/stolostron/multicluster-global-hub/pkg/kafka/headers"
 	"github.com/stolostron/multicluster-global-hub/test/pkg/testkafka"
 	"github.com/stolostron/multicluster-global-hub/test/pkg/testpostgres"
 )
@@ -79,12 +83,12 @@ var _ = BeforeSuite(func() {
 	stats, err := statistics.NewStatistics(ctrl.Log.WithName("statistics"), &statistics.StatisticsConfig{})
 	Expect(err).NotTo(HaveOccurred())
 	dbWorkerPool, err = workerpool.NewDBWorkerPool(ctrl.Log.WithName("db-worker-pool"), testPostgres.URI, stats)
+	Expect(err).NotTo(HaveOccurred())
 	Expect(dbWorkerPool.Start()).Should(Succeed())
 
 	By("Create conflationReadyQueue")
 	conflationReadyQueue := conflator.NewConflationReadyQueue(stats)
-	conflationManager := conflator.NewConflationManager(ctrl.Log.WithName("conflation"), conflationReadyQueue,
-		false, stats) // manage all Conflation Units
+	conflationManager := conflator.NewConflationManager(ctrl.Log.WithName("conflation"), conflationReadyQueue, false, stats) // manage all Conflation Units
 
 	kafkaMessageChan = make(chan *kafka.Message)
 	statusTransport, err = testkafka.NewKafkaTestConsumer(kafkaMessageChan, conflationManager, stats,
@@ -103,6 +107,10 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(kubeClient).NotTo(BeNil())
 
+	By("Add to Scheme")
+	Expect(managerscheme.AddToScheme(mgr.GetScheme())).NotTo(HaveOccurred())
+
+	By("Create the global hub ConfigMap with aggregationLevel=full and enableLocalPolicies=true")
 	mghSystemNamespace := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: constants.HohSystemNamespace}}
 	Expect(kubeClient.Create(ctx, mghSystemNamespace)).Should(Succeed())
 	mghSystemConfigMap := &v1.ConfigMap{
@@ -114,17 +122,14 @@ var _ = BeforeSuite(func() {
 	}
 	Expect(kubeClient.Create(ctx, mghSystemConfigMap)).Should(Succeed())
 
-	By("Add to Scheme")
-	Expect(managerscheme.AddToScheme(mgr.GetScheme())).NotTo(HaveOccurred())
+	By("Add controllers to manager")
+	err = statussyncer.AddTransport2DBSyncers(mgr, dbWorkerPool, conflationManager, conflationReadyQueue, statusTransport, stats)
+	Expect(err).ToNot(HaveOccurred())
 
-	By("Add transport to database controller")
-	err = statussyncer.AddTransport2DBSyncers(mgr, dbWorkerPool, conflationManager,
-		conflationReadyQueue, statusTransport, stats)
-	Expect(err).NotTo(HaveOccurred())
+	By("Start the manager")
 	go func() {
 		defer GinkgoRecover()
-		err = mgr.Start(ctx)
-		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+		Expect(mgr.Start(ctx)).ToNot(HaveOccurred(), "failed to run manager")
 	}()
 
 	By("Waiting for the manager to be ready")
@@ -132,14 +137,13 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = AfterSuite(func() {
-	By("tearing down the test environment")
-
 	cancel()
 	statusTransport.Stop()
 	transportPostgreSQL.Stop()
 	dbWorkerPool.Stop()
 	Expect(testPostgres.Stop()).NotTo(HaveOccurred())
 
+	By("Tearing down the test environment")
 	err := testenv.Stop()
 	// https://github.com/kubernetes-sigs/controller-runtime/issues/1571
 	// Set 4 with random
@@ -148,3 +152,42 @@ var _ = AfterSuite(func() {
 	}
 	Expect(testenv.Stop()).NotTo(HaveOccurred())
 })
+
+func buildKafkaMessage(key, id string, payload []byte) (*kafka.Message, error) {
+	transportMessage := &producer.Message{
+		Key:     key,
+		ID:      id, // entry.transportBundleKey
+		MsgType: constants.StatusBundle,
+		Version: "0.2", // entry.bundle.GetBundleVersion().String()
+		Payload: payload,
+	}
+	transportMessageBytes, err := json.Marshal(transportMessage)
+	if err != nil {
+		return nil, err
+	}
+
+	compressor, err := compressor.NewCompressor(compressor.GZip)
+	if err != nil {
+		return nil, err
+	}
+	compressedTransportBytes, err := compressor.Compress(transportMessageBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	topic := "status"
+	kafkaMessage := &kafka.Message{
+		Key: []byte(transportMessage.Key),
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: 0,
+		},
+		Headers: []kafka.Header{
+			{Key: headers.CompressionType, Value: []byte(compressor.GetType())},
+		},
+		Value:         compressedTransportBytes,
+		TimestampType: 1,
+		Timestamp:     time.Now(),
+	}
+	return kafkaMessage, nil
+}
