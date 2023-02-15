bc4012e49a7418609c0f3d72b4bc0d9cddfe3684
myan <myan@redhat.com>
2023-02-15 09:44:57 +0000

    add agent spce unit tests
    Signed-off-by: myan <myan@redhat.com>

---
+++ agent/pkg/spec/controller/syncers/syncers_integration_test.go
@@ -0,0 +1,127 @@
+package syncers_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/stolostron/multicluster-global-hub/pkg/bundle/spec"
+	"github.com/stolostron/multicluster-global-hub/pkg/constants"
+	"github.com/stolostron/multicluster-global-hub/pkg/transport"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterv1 "open-cluster-management.io/api/cluster/v1"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/stolostron/multicluster-global-hub/manager/pkg/specsyncer/db2transport/bundle"
+)
+
+const timeFormat = "2006-01-02_15-04-05.000000"
+
+var _ = Describe("Syncers Integration", func() {
+	It("sync managedclusterlabel bundle", func() {
+		managedClusterName := "mc1"
+
+		By("Create ManagedCluster on the regional hub")
+		managedCluster := clusterv1.ManagedCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: managedClusterName,
+				Labels: map[string]string{
+					"vendor": "OpenShift",
+				},
+			},
+			Spec: clusterv1.ManagedClusterSpec{
+				HubAcceptsClient: true,
+			},
+		}
+		Expect(client.Create(ctx, &managedCluster)).NotTo(HaveOccurred())
+
+		Eventually(func() error {
+			mc := clusterv1.ManagedCluster{}
+			return client.Get(ctx, runtimeclient.ObjectKeyFromObject(&managedCluster), &mc)
+		}, 5*time.Second, 100*time.Millisecond).ShouldNot(HaveOccurred())
+
+		By("Create ManagedClusterLabelBundle with labels")
+		managedClusterLabelsSpecBundle := &spec.ManagedClusterLabelsSpecBundle{
+			Objects:     []*spec.ManagedClusterLabelsSpec{},
+			LeafHubName: agentConfig.LeafHubName,
+		}
+		bundleObj := &spec.ManagedClusterLabelsSpec{
+			ClusterName: managedClusterName,
+			Labels: map[string]string{
+				"test": "add",
+			},
+			DeletedLabelKeys: []string{},
+			Version:          10,
+			UpdateTimestamp:  time.Now(),
+		}
+		managedClusterLabelsSpecBundle.Objects = append(managedClusterLabelsSpecBundle.Objects, bundleObj)
+
+		By("Send ManagedClusterLabelBundle by transport")
+		payloadBytes, err := json.Marshal(managedClusterLabelsSpecBundle)
+		Expect(err).NotTo(HaveOccurred())
+		err = producer.Send(ctx, &transport.Message{
+			Destination: agentConfig.LeafHubName,
+			ID:          constants.ManagedClustersLabelsMsgKey,
+			MsgType:     constants.SpecBundle,
+			Version:     time.Now().Format(timeFormat),
+			Payload:     payloadBytes,
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		By("Check the managed cluster label")
+		Eventually(func() error {
+			mc := clusterv1.ManagedCluster{}
+			err = client.Get(ctx, runtimeclient.ObjectKeyFromObject(&managedCluster), &mc)
+			if err != nil {
+				return err
+			}
+			val := mc.GetLabels()["test"]
+			if val == "add" {
+				fmt.Println(mc.Labels)
+				return nil
+			}
+			return fmt.Errorf("not found label { %s : %s}", "test", "add")
+		}, 5*time.Second, 100*time.Microsecond).ShouldNot(HaveOccurred())
+	})
+
+	It("sync configmap bundle", func() {
+		By("Create Config Bundle")
+		baseBundle := bundle.NewBaseObjectsBundle()
+		cm := &corev1.ConfigMap{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "ConfigMap",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "hello",
+				Namespace: "default",
+			},
+			Data: map[string]string{
+				"hello": "world",
+			},
+		}
+		baseBundle.AddObject(cm, uuid.New().String())
+
+		By("Send Config Bundle by transport")
+		payloadBytes, err := json.Marshal(baseBundle)
+		Expect(err).NotTo(HaveOccurred())
+		err = producer.Send(ctx, &transport.Message{
+			Destination: transport.Broadcast,
+			ID:          "Config",
+			MsgType:     constants.SpecBundle,
+			Version:     time.Now().Format(timeFormat),
+			Payload:     payloadBytes,
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		By("Check the configmap is synced")
+		Eventually(func() error {
+			syncedConfigMap := &corev1.ConfigMap{}
+			return client.Get(ctx, runtimeclient.ObjectKeyFromObject(cm), syncedConfigMap)
+		}, 5*time.Second, 100*time.Millisecond).ShouldNot(HaveOccurred())
+	})
+})
---
+++ agent/pkg/spec/controller/syncers/syncers_suite_test.go
@@ -0,0 +1,97 @@
+package syncers_test
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/stolostron/multicluster-global-hub/agent/pkg/config"
+	agentscheme "github.com/stolostron/multicluster-global-hub/agent/pkg/scheme"
+	speccontroller "github.com/stolostron/multicluster-global-hub/agent/pkg/spec/controller"
+	"github.com/stolostron/multicluster-global-hub/pkg/transport"
+	genericproducer "github.com/stolostron/multicluster-global-hub/pkg/transport/producer"
+)
+
+func TestSyncers(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Spec Syncers Suite")
+}
+
+var (
+	testenv     *envtest.Environment
+	cfg         *rest.Config
+	agentConfig *config.AgentConfig
+	ctx         context.Context
+	cancel      context.CancelFunc
+	client      runtimeclient.Client
+	producer    transport.Producer
+)
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	ctx, cancel = context.WithCancel(context.Background())
+
+	testenv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "..", "..", "..", "..", "pkg", "testdata", "crds"),
+		},
+	}
+
+	var err error
+	cfg, err = testenv.Start()
+	Expect(err).NotTo(HaveOccurred())
+
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{})
+	Expect(err).NotTo(HaveOccurred())
+
+	// add scheme
+	Expect(agentscheme.AddToScheme(mgr.GetScheme())).NotTo(HaveOccurred())
+
+	agentConfig = &config.AgentConfig{
+		TransportConfig: &transport.TransportConfig{
+			TransportType: string(transport.Chan),
+		},
+		SpecWorkPoolSize:   2,
+		LeafHubName:        "leaf-hub1",
+		SpecEnforceHohRbac: true,
+	}
+
+	err = speccontroller.AddToManager(mgr, agentConfig)
+	Expect(err).NotTo(HaveOccurred())
+
+	go func() {
+		Expect(mgr.Start(ctx)).NotTo(HaveOccurred())
+	}()
+
+	By("Waiting for the manager to be ready")
+	Expect(mgr.GetCache().WaitForCacheSync(ctx)).To(BeTrue())
+
+	client = mgr.GetClient()
+
+	producer, err = genericproducer.NewGenericProducer(agentConfig.TransportConfig)
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	cancel()
+
+	By("Tearing down the test environment")
+	err := testenv.Stop()
+	// https://github.com/kubernetes-sigs/controller-runtime/issues/1571
+	// Set 4 with random
+	if err != nil {
+		time.Sleep(4 * time.Second)
+	}
+	Expect(testenv.Stop()).NotTo(HaveOccurred())
+})
