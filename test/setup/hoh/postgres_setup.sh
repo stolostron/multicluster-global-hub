1517e6df897b4066bb70fed9d51aa46c03a78988
myan <myan@redhat.com>
2023-04-23 06:36:40 +0000

    enable HA for the postgres cluster
    Signed-off-by: myan <myan@redhat.com>

--- test/setup/hoh/postgres_setup.sh
+++ test/setup/hoh/postgres_setup.sh
@@ -10,6 +10,10 @@ bash $postgresDir/deploy_postgres.sh $KUBECONFIG
 # expose the postgres service as NodePort
 pgnamespace="hoh-postgres"
 kubectl patch postgrescluster hoh -n $pgnamespace -p '{"spec":{"service":{"type":"NodePort", "nodePort": 32432}}}'  --type merge
+# unset HA
+kubectl patch configmap pgo-config -n hoh-postgres --type json -p='[{"op": "replace", "path": "/data/DisableAutofail", "value": "true"}]'
+kubectl patch postgrescluster hoh -n hoh-postgres --type json -p='[{"op": "replace", "path": "/spec/instances/0/replicas", "value": 1}]'
+kubectl patch postgrescluster hoh -n hoh-postgres --type merge --patch '{"spec":{"proxy":{"pgBouncer":{"replicas":1}}}}'
 
 stss=$(kubectl get statefulset -n $pgnamespace -o jsonpath={.items..metadata.name})
 for sts in ${stss}; do
