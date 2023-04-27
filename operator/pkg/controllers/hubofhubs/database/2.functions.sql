074027bb8974d7d18543d6447cdcfed87de72e99
clyang82 <chuyang@redhat.com>
2023-04-27 23:07:36 +0800

    Address comments
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- operator/pkg/controllers/hubofhubs/database/2.functions.sql
+++ operator/pkg/controllers/hubofhubs/database/2.functions.sql
@@ -288,11 +288,22 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION public.set_cluster_id_to_compliance() RETURNS trigger
+CREATE OR REPLACE FUNCTION public.set_cluster_id_to_local_compliance() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 BEGIN
   UPDATE local_status.compliance set cluster_id=(SELECT cluster_id FROM status.managed_clusters
+  WHERE payload -> 'metadata' ->> 'name' = NEW.cluster_name AND leaf_hub_name = NEW.leaf_hub_name)
+  WHERE cluster_name = NEW.cluster_name AND leaf_hub_name = NEW.leaf_hub_name AND cluster_id IS NULL;
+  RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION public.set_cluster_id_to_compliance() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  UPDATE status.compliance set cluster_id=(SELECT cluster_id FROM status.managed_clusters
   WHERE payload -> 'metadata' ->> 'name' = NEW.cluster_name AND leaf_hub_name = NEW.leaf_hub_name)
   WHERE cluster_name = NEW.cluster_name AND leaf_hub_name = NEW.leaf_hub_name AND cluster_id IS NULL;
   RETURN NEW;
