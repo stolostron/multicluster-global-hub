23afe83c861fee2528a396030e3c2f08c4ea20cf
clyang82 <chuyang@redhat.com>
2023-05-16 15:39:13 +0800

    update trigger statement
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- operator/pkg/controllers/hubofhubs/database/4.trigger.sql
+++ operator/pkg/controllers/hubofhubs/database/4.trigger.sql
@@ -68,9 +68,11 @@ CREATE TRIGGER set_timestamp BEFORE UPDATE ON spec.policies FOR EACH ROW EXECUTE
 DROP TRIGGER IF EXISTS set_timestamp ON spec.subscriptions;
 CREATE TRIGGER set_timestamp BEFORE UPDATE ON spec.subscriptions FOR EACH ROW EXECUTE FUNCTION public.trigger_set_timestamp();
 
-DROP TRIGGER IF EXISTS update_compliance_table ON local_status.compliance;
+DROP TRIGGER IF EXISTS update_compliance_table_after_insert ON local_status.compliance;
 CREATE TRIGGER update_compliance_table_after_insert AFTER INSERT ON local_status.compliance FOR EACH ROW EXECUTE FUNCTION public.set_cluster_id_to_local_compliance();
+DROP TRIGGER IF EXISTS update_compliance_table_before_update ON local_status.compliance;
 CREATE TRIGGER update_compliance_table_before_update BEFORE UPDATE ON local_status.compliance FOR EACH ROW EXECUTE FUNCTION public.set_cluster_id_to_local_compliance();
-DROP TRIGGER IF EXISTS update_compliance_table ON status.compliance;
+DROP TRIGGER IF EXISTS update_compliance_table_after_insert ON status.compliance;
 CREATE TRIGGER update_compliance_table_after_insert AFTER INSERT ON status.compliance FOR EACH ROW EXECUTE FUNCTION public.set_cluster_id_to_compliance();
+DROP TRIGGER IF EXISTS update_compliance_table_before_update ON status.compliance;
 CREATE TRIGGER update_compliance_table_before_update BEFORE UPDATE ON status.compliance FOR EACH ROW EXECUTE FUNCTION public.set_cluster_id_to_compliance();
