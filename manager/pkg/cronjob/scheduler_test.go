e5148695478f833febc1810fb087d453f76ca014
myan <myan@redhat.com>
2023-05-26 14:20:10 +0000

    fixed sunar error
    Signed-off-by: myan <myan@redhat.com>

--- manager/pkg/cronjob/scheduler_test.go
+++ manager/pkg/cronjob/scheduler_test.go
@@ -12,19 +12,15 @@ import (
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-
-	"github.com/stolostron/multicluster-global-hub/pkg/database"
-	"github.com/stolostron/multicluster-global-hub/test/pkg/testpostgres"
 )
 
 var (
-	testenv      *envtest.Environment
-	cfg          *rest.Config
-	ctx          context.Context
-	cancel       context.CancelFunc
-	mgr          ctrl.Manager
-	pool         *pgxpool.Pool
-	testPostgres *testpostgres.TestPostgres
+	testenv *envtest.Environment
+	cfg     *rest.Config
+	ctx     context.Context
+	cancel  context.CancelFunc
+	mgr     ctrl.Manager
+	pool    *pgxpool.Pool
 )
 
 func TestScheduler(t *testing.T) {
@@ -40,27 +36,19 @@ func TestScheduler(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, cfg)
 
-	testPostgres, err = testpostgres.NewTestPostgres()
-	assert.Nil(t, err)
-
-	pool, err = database.PostgresConnPool(ctx, testPostgres.URI, "ca-cert-path")
-	assert.Nil(t, err)
-
 	mgr, err = ctrl.NewManager(cfg, ctrl.Options{
 		MetricsBindAddress: "0",
 		Scheme:             scheme.Scheme,
 	})
 	assert.Nil(t, err)
 
-	assert.Nil(t, AddSchedulerToManager(context.TODO(), mgr, pool, "month", false))
-	assert.Nil(t, AddSchedulerToManager(context.TODO(), mgr, pool, "week", false))
-	assert.Nil(t, AddSchedulerToManager(context.TODO(), mgr, pool, "day", false))
-	assert.Nil(t, AddSchedulerToManager(context.TODO(), mgr, pool, "hour", false))
-	assert.Nil(t, AddSchedulerToManager(context.TODO(), mgr, pool, "minute", false))
-	assert.Nil(t, AddSchedulerToManager(context.TODO(), mgr, pool, "second", false))
+	assert.Nil(t, AddSchedulerToManager(ctx, mgr, pool, "month", false))
+	assert.Nil(t, AddSchedulerToManager(ctx, mgr, pool, "week", false))
+	assert.Nil(t, AddSchedulerToManager(ctx, mgr, pool, "day", false))
+	assert.Nil(t, AddSchedulerToManager(ctx, mgr, pool, "hour", false))
+	assert.Nil(t, AddSchedulerToManager(ctx, mgr, pool, "minute", false))
+	assert.Nil(t, AddSchedulerToManager(ctx, mgr, pool, "second", false))
 
-	err = testPostgres.Stop()
-	assert.Nil(t, err)
 	cancel()
 	err = testenv.Stop()
 	// https://github.com/kubernetes-sigs/controller-runtime/issues/1571
