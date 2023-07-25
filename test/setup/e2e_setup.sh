573d5b0a084db1a3003b424044523b68af21eb37
clyang82 <chuyang@redhat.com>
2023-07-25 14:52:17 +0800

    Test
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- test/setup/e2e_setup.sh
+++ test/setup/e2e_setup.sh
@@ -23,11 +23,16 @@ export KUBECONFIG=${KUBECONFIG:-${CONFIG_DIR}/kubeconfig}
 echo "export KUBECONFIG=$KUBECONFIG" > $LOG
 sleep 1 &
 hover $! "KUBECONFIG=${KUBECONFIG}"
+startTime_s=`date +%s`
 
-# init hoh 
+# init hoh
 source ${CURRENT_DIR}/microshift/microshift_setup.sh "$HUB_OF_HUB_NAME" 2>&1 >> $LOG &
 hover $! "1 Prepare top hub cluster $HUB_OF_HUB_NAME"
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "Prepare top hub :$sumTime seconds"
 
+startTime_s=`date +%s`
 # isolate the hub kubeconfig
 HOH_KUBECONFIG=${CONFIG_DIR}/kubeconfig-hub-${CTX_HUB} # kind get kubeconfig --name "$HUB_OF_HUB_NAME" --internal > "$HOH_KUBECONFIG"
 kubectl config view --context=${CTX_HUB} --minify --flatten > ${HOH_KUBECONFIG}
@@ -35,53 +40,75 @@ kubectl config view --context=${CTX_HUB} --minify --flatten > ${HOH_KUBECONFIG}
 # enable olm
 enableOLM $CTX_HUB 2>&1 >> $LOG &
 hover $! "  Enable OLM for $CTX_HUB"
-
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "Enable OLM :$sumTime seconds"
+startTime_s=`date +%s`
 # install some component in microshift in detached mode
 bash ${CURRENT_DIR}/hoh/postgres_setup.sh $HOH_KUBECONFIG 2>&1 >> $LOG &
 bash ${CURRENT_DIR}/hoh/kafka_setup.sh $HOH_KUBECONFIG 2>&1 >> $LOG &
 initHub $CTX_HUB 2>&1 >> $LOG &
-
-# init leafhub 
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "initHub :$sumTime seconds"
+startTime_s=`date +%s`
+# init leafhub
 sleep 1 &
 hover $! "2 Prepare leaf hub cluster $LEAF_HUB_NAME"
 source ${CURRENT_DIR}/leafhub_setup.sh "$HUB_CLUSTER_NUM" "$MANAGED_CLUSTER_NUM"
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "Prepare leaf hub cluster :$sumTime seconds"
 
+startTime_s=`date +%s`
 # joining lh to hoh
 initHub $CTX_HUB 2>&1 >> $LOG &
-hover $! "3 Init HoH OCM $HUB_OF_HUB_NAME" 
+hover $! "3 Init HoH OCM $HUB_OF_HUB_NAME"
 
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "Init HoH OCM :$sumTime seconds"
+startTime_s=`date +%s`
 # check connection
 for i in $(seq 1 "${HUB_CLUSTER_NUM}"); do
     connectMicroshift "${LEAF_HUB_NAME}${i}" "${HUB_OF_HUB_NAME}" 2>&1 >> $LOG &
-    hover $! "  Check connection: $LEAF_HUB_NAME$i -> $HUB_OF_HUB_NAME" 
+    hover $! "  Check connection: $LEAF_HUB_NAME$i -> $HUB_OF_HUB_NAME"
 
     initManaged $CTX_HUB $CTX_MANAGED$i 2>&1 >> $LOG &
-    hover $! "  Joining $CTX_HUB - $CTX_MANAGED$i" 
+    hover $! "  Joining $CTX_HUB - $CTX_MANAGED$i"
     checkManagedCluster $CTX_HUB $CTX_MANAGED$i 2>&1 >> $LOG
 
     initApp $CTX_HUB $CTX_MANAGED$i 2>&1 >> $LOG &
-    hover $! "  Enable application $CTX_HUB - $CTX_MANAGED$i" 
+    hover $! "  Enable application $CTX_HUB - $CTX_MANAGED$i"
 
     initPolicy $CTX_HUB $CTX_MANAGED$i $HOH_KUBECONFIG 2>&1 >> $LOG &
-    hover $! "  Enable Policy $CTX_HUB - $CTX_MANAGED$i" 
+    hover $! "  Enable Policy $CTX_HUB - $CTX_MANAGED$i"
 
     kubectl config use-context $CTX_HUB >> $LOG
 done
-
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "check connection :$sumTime seconds"
+startTime_s=`date +%s`
 # wait kafka to be ready
 waitAppear "kubectl get pods -n kafka -l name=strimzi-cluster-operator --ignore-not-found | grep Running || true" 1200
 waitAppear "kubectl get kafka kafka-brokers-cluster -n kafka -o jsonpath='{.status.listeners[1].certificates[0]}' --ignore-not-found=true" 1200
 
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "wait kafka :$sumTime seconds"
 # wait postgres to be ready
 waitAppear "kubectl get secret hoh-pguser-postgres -n hoh-postgres --ignore-not-found=true"
-
+startTime_s=`date +%s`
 #deploy hoh
 #need the following labels to enable deploying agent in leaf hub cluster
 for i in $(seq 1 "${HUB_CLUSTER_NUM}"); do
-    kubectl label managedcluster kind-$LEAF_HUB_NAME$i vendor=OpenShift --overwrite 2>&1 >> $LOG 
+    kubectl label managedcluster kind-$LEAF_HUB_NAME$i vendor=OpenShift --overwrite 2>&1 >> $LOG
 done
 source ${CURRENT_DIR}/hoh/hoh_setup.sh >> $LOG 2>&1 &
-hover $! "6 Deploy hub-of-hubs with $TAG" 
-
+hover $! "6 Deploy hub-of-hubs with $TAG"
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "deploy hoh :$sumTime seconds"
 export KUBECONFIG=$KUBECONFIG
 printf "%s\033[0;32m%s\n\033[0m" "[Access the Clusters]: " "export KUBECONFIG=$KUBECONFIG"
