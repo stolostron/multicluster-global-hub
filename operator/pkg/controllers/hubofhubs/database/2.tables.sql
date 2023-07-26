932c207ad94636e46f14bf60319ea7480aa1049b
myan <myan@redhat.com>
2023-07-26 02:33:37 +0000

    fixed the ut
    Signed-off-by: myan <myan@redhat.com>

--- operator/pkg/controllers/hubofhubs/database/2.tables.sql
+++ operator/pkg/controllers/hubofhubs/database/2.tables.sql
@@ -117,7 +117,7 @@ CREATE TABLE IF NOT EXISTS local_spec.policies (
     policy_category character varying(255) generated always as (payload -> 'metadata' -> 'annotations' ->> 'policy.open-cluster-management.io/categories') stored,
     policy_control character varying(255) generated always as (payload -> 'metadata' -> 'annotations' ->> 'policy.open-cluster-management.io/controls') stored
 );
-CREATE INDEX local_policies_deleted_at_idx ON status.leaf_hubs (deleted_at);
+CREATE INDEX local_policies_deleted_at_idx ON local_spec.policies (deleted_at);
 
 CREATE TABLE IF NOT EXISTS local_status.compliance (
     policy_id uuid NOT NULL,
@@ -257,7 +257,7 @@ CREATE TABLE IF NOT EXISTS status.managed_clusters (
     updated_at timestamp without time zone DEFAULT now() NOT NULL,
     deleted_at timestamp without time zone
 );
-CREATE INDEX status.cluster_deleted_at_idx ON status.managed_clusters (deleted_at);
+CREATE INDEX cluster_deleted_at_idx ON status.managed_clusters (deleted_at);
 
 CREATE TABLE IF NOT EXISTS status.leaf_hubs (
     leaf_hub_name character varying(63) NOT NULL PRIMARY KEY,
