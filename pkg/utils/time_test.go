9d83d1e1b435cc632b4223d7dfd7f2818da5ea03
myan <myan@redhat.com>
2023-07-28 02:47:17 +0000

    conver the error case
    Signed-off-by: myan <myan@redhat.com>

--- pkg/utils/time_test.go
+++ pkg/utils/time_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -44,13 +45,25 @@ func TestParseDuration(t *testing.T) {
 			expected: 2*time.Hour + 3*24*time.Hour + 4*30*24*time.Hour + 5*365*24*time.Hour,
 			err:      nil,
 		},
+		{
+			name:     "invalid duration with non-numeric character",
+			input:    "-..2h3d4m5y",
+			expected: 0,
+			err:      errors.New(InvalidDurationMessage + quote("-..2h3d4m5y")),
+		},
+		{
+			name:     "invalid duration with non-ASCII character",
+			input:    "\u263A",
+			expected: 0,
+			err:      errors.New(InvalidDurationMessage + quote("\u263A")),
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			duration, err := ParseDuration(tc.input)
-			assert.Equal(t, tc.expected, duration)
 			assert.Equal(t, tc.err, err)
+			assert.Equal(t, tc.expected, duration)
 		})
 	}
 }
