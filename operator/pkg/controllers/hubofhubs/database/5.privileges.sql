930aea3d2d4d380164b4ec8b5cbefd155821e40d
clyang82 <chuyang@redhat.com>
2023-08-11 13:25:30 +0800

    Get the readonly user name from secret
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- operator/pkg/controllers/hubofhubs/database/5.privileges.sql
+++ operator/pkg/controllers/hubofhubs/database/5.privileges.sql
@@ -1,13 +1,18 @@
-GRANT USAGE ON SCHEMA spec TO guest;
-GRANT USAGE ON SCHEMA status TO guest;
-GRANT USAGE ON SCHEMA event TO guest;
-GRANT USAGE ON SCHEMA history TO guest;
-GRANT USAGE ON SCHEMA local_spec TO guest;
-GRANT USAGE ON SCHEMA local_status TO guest;
+-- grant privileges to a readonly user
+DO $$ BEGIN
+    IF EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$1') THEN
+        GRANT USAGE ON SCHEMA spec TO $1;
+        GRANT USAGE ON SCHEMA status TO $1;
+        GRANT USAGE ON SCHEMA event TO $1;
+        GRANT USAGE ON SCHEMA history TO $1;
+        GRANT USAGE ON SCHEMA local_spec TO $1;
+        GRANT USAGE ON SCHEMA local_status TO $1;
 
-GRANT SELECT ON ALL TABLES IN SCHEMA spec TO guest;
-GRANT SELECT ON ALL TABLES IN SCHEMA status TO guest;
-GRANT SELECT ON ALL TABLES IN SCHEMA event TO guest;
-GRANT SELECT ON ALL TABLES IN SCHEMA history TO guest;
-GRANT SELECT ON ALL TABLES IN SCHEMA local_spec TO guest;
-GRANT SELECT ON ALL TABLES IN SCHEMA local_status TO guest;
+        GRANT SELECT ON ALL TABLES IN SCHEMA spec TO $1;
+        GRANT SELECT ON ALL TABLES IN SCHEMA status TO $1;
+        GRANT SELECT ON ALL TABLES IN SCHEMA event TO $1;
+        GRANT SELECT ON ALL TABLES IN SCHEMA history TO $1;
+        GRANT SELECT ON ALL TABLES IN SCHEMA local_spec TO $1;
+        GRANT SELECT ON ALL TABLES IN SCHEMA local_status TO $1;
+   END IF;
+END $$;
