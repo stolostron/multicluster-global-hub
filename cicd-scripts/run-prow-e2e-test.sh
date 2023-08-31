95f76aa7e023769e71e1dec4d420787306db4a35
myan <myan@redhat.com>
2023-08-31 14:45:39 +0000

    remove the wait
    Signed-off-by: myan <myan@redhat.com>

--- cicd-scripts/run-prow-e2e-test.sh
+++ cicd-scripts/run-prow-e2e-test.sh
@@ -28,6 +28,5 @@ ssh "${OPT[@]}" "$HOST" "sudo sh -c 'echo \"fs.inotify.max_user_watches=524288\"
 echo "setup e2e environment"
 ssh "${OPT[@]}" "$HOST" "cd $HOST_DIR && . test/resources/env.list && sudo make e2e-setup-dependencies && make e2e-setup-start" > >(tee "$ARTIFACT_DIR/run-e2e-setup.log") 2>&1
 
-sleep 3600
 echo "runn e2e tests"
 ssh "${OPT[@]}" "$HOST" "cd $HOST_DIR && . test/resources/env.list && make e2e-tests-all && make e2e-tests-prune" > >(tee "$ARTIFACT_DIR/run-e2e-test.log") 2>&1
