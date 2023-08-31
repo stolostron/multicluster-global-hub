deb679bc4b26cd8cf1bfa596aa6dff60bfc810bd
myan <myan@redhat.com>
2023-08-31 05:20:19 +0000

    upgrade the placmentbinding crds
    Signed-off-by: myan <myan@redhat.com>

--- test/setup/common.sh
+++ test/setup/common.sh
@@ -136,7 +136,8 @@ function initPolicy() {
     GIT_PATH="https://raw.githubusercontent.com/open-cluster-management-io/governance-policy-propagator/v0.11.0/deploy"
     ## Apply the CRDs
     kubectl --context "${hub}" apply -f ${GIT_PATH}/crds/policy.open-cluster-management.io_policies.yaml 
-    kubectl --context "${hub}" apply -f ${GIT_PATH}/crds/policy.open-cluster-management.io_placementbindings.yaml 
+    ## Upgrade the PlacementBinding CRD https://github.com/open-cluster-management-io/governance-policy-propagator/commit/0d164a21ea1272fc2e090b2e7d54853756cb270b
+    kubectl --context "${hub}" apply -f https://raw.githubusercontent.com/open-cluster-management-io/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_placementbindings.yaml 
     kubectl --context "${hub}" apply -f ${GIT_PATH}/crds/policy.open-cluster-management.io_policyautomations.yaml
     kubectl --context "${hub}" apply -f ${GIT_PATH}/crds/policy.open-cluster-management.io_policysets.yaml
     ## Deploy the policy-propagator
