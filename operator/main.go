b6da2d88a3e5f0d2ba0bad9b20cdb4e25a21daca
clyang82 <chuyang@redhat.com>
2023-09-26 15:01:27 +0800

    Test sonarcloud
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- operator/main.go
+++ operator/main.go
@@ -265,7 +265,7 @@ func getManager(restConfig *rest.Config, electionConfig *commonobjects.LeaderEle
 		Port:                    9443,
 		HealthProbeBindAddress:  operatorConfig.ProbeAddress,
 		LeaderElection:          operatorConfig.LeaderElection,
-		LeaderElectionID:        "549a8919.open-cluster-management.io",
+		LeaderElectionID:        "multicluster-global-hub-operator-lock",
 		LeaderElectionNamespace: operatorConfig.PodNamespace,
 		LeaseDuration:           &leaseDuration,
 		RenewDeadline:           &renewDeadline,
