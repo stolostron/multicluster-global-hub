0769e0b226a126574b1049da259920804d2041ec
clyang82 <chuyang@redhat.com>
2023-10-11 17:07:05 +0800

    Trigger reconcile when the annotation of mcgh is changed
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- operator/pkg/controllers/hubofhubs/globalhub_controller.go
+++ operator/pkg/controllers/hubofhubs/globalhub_controller.go
@@ -317,7 +317,7 @@ var mghPred = predicate.Funcs{
 		return true
 	},
 	UpdateFunc: func(e event.UpdateEvent) bool {
-		return e.ObjectOld.GetGeneration() != e.ObjectNew.GetGeneration()
+		return e.ObjectOld.GetResourceVersion() != e.ObjectNew.GetResourceVersion()
 	},
 	DeleteFunc: func(e event.DeleteEvent) bool {
 		return !e.DeleteStateUnknown
