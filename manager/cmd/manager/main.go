16313386338c9e386740862dcc7bddfe4b54a5e7
Meng Yan <myan@redhat.com>
2023-10-12 04:34:14 -0500

    Remove the duplicate metrics under a high availability condition (#673)
    * sonarcloud
    
    Signed-off-by: Meng Yan <myan@redhat.com>
    
    * extend the timeout of cronjob
    
    Signed-off-by: Meng Yan <myan@redhat.com>
    
    * remove the duplicate metrics under a high availability condition
    
    Signed-off-by: Meng Yan <myan@redhat.com>
    
    * upgrade log level
    
    Signed-off-by: Meng Yan <myan@redhat.com>
    
    ---------
    
    Signed-off-by: Meng Yan <myan@redhat.com>

--- manager/cmd/manager/main.go
+++ manager/cmd/manager/main.go
@@ -24,6 +24,7 @@ import (
 	managerconfig "github.com/stolostron/multicluster-global-hub/manager/pkg/config"
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/cronjob"
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/eventcollector"
+	globalhubmetrics "github.com/stolostron/multicluster-global-hub/manager/pkg/metrics"
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/nonk8sapi"
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/scheme"
 	"github.com/stolostron/multicluster-global-hub/manager/pkg/specsyncer"
@@ -244,6 +245,10 @@ func createManager(ctx context.Context, restConfig *rest.Config, managerConfig *
 	eventKafkaConfig.ConsumerConfig.ConsumerTopic = managerConfig.EventExporterTopic
 	if err := eventcollector.AddEventCollector(ctx, mgr, eventKafkaConfig); err != nil {
 		return nil, fmt.Errorf("failed to add event collector: %w", err)
+	}
+
+	if err := mgr.Add(globalhubmetrics.NewGlobalHubMetrics()); err != nil {
+		return nil, fmt.Errorf("failed to add metrics to manager: %w", err)
 	}
 
 	return mgr, nil
