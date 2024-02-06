212b655c4f2f44da5ca0aa8710da9add6503aa75
myan <myan@redhat.com>
2024-02-07 07:56:31 +0800

    fix the sunor
    Signed-off-by: myan <myan@redhat.com>

--- pkg/enum/event_type.go
+++ pkg/enum/event_type.go
@@ -3,8 +3,6 @@ package enum
 type EventType string
 
 const (
-	//lint:ignore go:S103
 	LocalReplicatedPolicyEventType EventType = "io.open-cluster-management.operator.multiclusterglobalhubs.local.replicatedpolicy.update"
-	//lint:ignore go:S103
-	LocalRootPolicyEventType EventType = "io.open-cluster-management.operator.multiclusterglobalhubs.local.policy.propagate"
+	LocalRootPolicyEventType       EventType = "io.open-cluster-management.operator.multiclusterglobalhubs.local.policy.propagate"
 )
