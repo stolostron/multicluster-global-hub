24380d6e237685259df923c10db89981e3030c58
myan <myan@redhat.com>
2024-03-12 16:38:03 +0800

    fix the ut
    Signed-off-by: myan <myan@redhat.com>

--- operator/pkg/controllers/hubofhubs/integration_test.go
+++ operator/pkg/controllers/hubofhubs/integration_test.go
@@ -931,12 +931,17 @@ var _ = Describe("MulticlusterGlobalHub controller", Ordered, func() {
 
 		It("Should get the postgres/kafka connection", func() {
 			Eventually(func() error {
+				if mghReconciler.MiddlewareConfig == nil {
+					mghReconciler.MiddlewareConfig = &hubofhubs.MiddlewareConfig{}
+				}
 				mghReconciler.MiddlewareConfig.StorageConn = nil
 				mghReconciler.MiddlewareConfig.TransportConn = nil
 				mghReconciler.ReconcileMiddleware(ctx, mcgh)
 
 				err := kafka.UpdateKafkaClusterReady(k8sClient, mcgh.Namespace)
-				Expect(err).NotTo(HaveOccurred())
+				if err != nil {
+					return err
+				}
 
 				// postgres should be ready in envtest
 				// kafka should be ready in envtest
