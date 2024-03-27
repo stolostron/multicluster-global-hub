47705367fc32a23da829ad04e43214ac2fa11347
myan <myan@redhat.com>
2024-03-27 23:10:18 +0800

    con
    Signed-off-by: myan <myan@redhat.com>

--- doc/simulation/local-policies/setup-policy.sh
+++ doc/simulation/local-policies/setup-policy.sh
@@ -22,6 +22,7 @@ CURRENT_DIR=$(cd "$(dirname "$0")" || exit;pwd)
 kubectl apply -f $REPO_DIR/pkg/testdata/crds/0000_00_policy.open-cluster-management.io_policies.crd.yaml
 kubectl apply -f $REPO_DIR/pkg/testdata/crds/0000_00_cluster.open-cluster-management.io_placements.crd.yaml
 kubectl apply -f $REPO_DIR/pkg/testdata/crds/0000_03_clusters.open-cluster-management.io_placementdecisions.crd.yaml
+max_concurrency=5
 
 source ${CURRENT_DIR}/policy.sh
 
@@ -42,11 +43,14 @@ function generate_replicas_policy() {
   limit_range_policy $rootpolicy_name &
 
   # create replicas policy: rootpolicy namespace, name and managed cluster
+  concurrency=0
   for j in $(seq $cluster_start $cluster_end); do
     cluster_name=managedcluster-${j}
     echo ">> Generating policy ${cluster_name}/${rootpolicy_name} on $KUBECONFIG"
 
-    limit_range_replicas_policy default $rootpolicy_name ${cluster_name}
+    limit_range_replicas_policy default $rootpolicy_name ${cluster_name} &
+    ((counter++))
+
     if [ $j == 1 ]; then
       status="{clustername: $cluster_name, clusternamespace: $cluster_name, compliant: NonCompliant}"
       decision="{clusterName: $cluster_name, reason: ''}"
@@ -54,15 +58,20 @@ function generate_replicas_policy() {
       status="${status}, {clustername: $cluster_name, clusternamespace: $cluster_name, compliant: NonCompliant}"
       decision="${decision}, {clusterName: $cluster_name, reason: ''}"
     fi
+    
+    if ((concurrency > max_concurrency)); then
+        wait
+        concurrency=0
+    fi
   done
+
+  wait
 
   # patch root policy status
   kubectl patch policy $rootpolicy_name -n default --type=merge --subresource status --patch "status: {compliant: NonCompliant, placement: [{placement: placement-$rootpolicy_name, placementBinding: binding-$rootpolicy_name}], status: [${status}]}" &
 
   # generate placement and placementdecision, each rootpolicy with a placement and placementdescision
   generate_placement default placement-$rootpolicy_name "$decision" &
-
-  # patch placementdecision status
   
   wait
 
