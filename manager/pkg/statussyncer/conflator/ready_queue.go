31cdb37d192ae8950dbde1a6229dd547e0d86b1b
Meng Yan <myan@redhat.com>
2024-03-29 18:39:49 +0000

    remove the code
    Signed-off-by: Meng Yan <myan@redhat.com>

--- manager/pkg/statussyncer/conflator/ready_queue.go
+++ manager/pkg/statussyncer/conflator/ready_queue.go
@@ -6,12 +6,8 @@ import (
 
 // NewConflationReadyQueue creates a new instance of ConflationReadyQueue.
 func NewConflationReadyQueue(statistics *statistics.Statistics) *ConflationReadyQueue {
-	// lock := &sync.Mutex{}
 
 	return &ConflationReadyQueue{
-		// queue:             list.New(),
-		// lock:              lock,
-		// notEmptyCondition: sync.NewCond(lock),
 		statistics:         statistics,
 		DeltaEventJobChan:  make(chan *ConflationJob, 1000),
 		ConflationUnitChan: make(chan *ConflationUnit, 100),
@@ -20,42 +16,8 @@ func NewConflationReadyQueue(statistics *statistics.Statistics) *ConflationReady
 
 // ConflationReadyQueue is a queue of conflation units that have at least one bundle to process.
 type ConflationReadyQueue struct {
-	// queue             *list.List
-	// lock              *sync.Mutex
-	// notEmptyCondition *sync.Cond
 	statistics *statistics.Statistics
 	// create a Job chan for the detal event
 	DeltaEventJobChan  chan *ConflationJob
 	ConflationUnitChan chan *ConflationUnit
 }
-
-// // Enqueue inserts ConflationUnit to the end of the ready queue.
-// func (rq *ConflationReadyQueue) Enqueue(cu *ConflationUnit) {
-// 	rq.lock.Lock()
-// 	defer rq.lock.Unlock()
-// 	rq.queue.PushBack(cu)
-// 	rq.notEmptyCondition.Signal() // Signal wakes another goroutine waiting on BlockingDequeue
-// 	rq.statistics.SetConflationReadyQueueSize(rq.queue.Len())
-// }
-
-// // BlockingDequeue pops ConflationUnit from the beginning of the queue. if no CU is ready, this call is blocking.
-// func (rq *ConflationReadyQueue) BlockingDequeue() *ConflationUnit {
-// 	rq.lock.Lock()
-// 	defer rq.lock.Unlock()
-// 	for rq.isEmpty() { // if ready rq is empty - wait
-// 		rq.notEmptyCondition.Wait() // wait until ready rq notEmptyCondition is true
-// 	}
-
-// 	conflationUnit, ok := rq.queue.Remove(rq.queue.Front()).(*ConflationUnit) // conflation unit is inside element.Value
-// 	rq.statistics.SetConflationReadyQueueSize(rq.queue.Len())
-
-// 	if !ok {
-// 		return nil
-// 	}
-
-// 	return conflationUnit
-// }
-
-// func (rq *ConflationReadyQueue) isEmpty() bool {
-// 	return rq.queue.Len() == 0
-// }
