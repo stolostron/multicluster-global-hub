28e070802b84ef99cfa5ba51b2f87ec8dac9a02b
myan <myan@redhat.com>
2024-05-09 01:40:11 +0000

    fix the sonar
    Signed-off-by: myan <myan@redhat.com>

--- pkg/utils/pprof.go
+++ pkg/utils/pprof.go
@@ -2,13 +2,18 @@ package utils
 
 import (
 	"net/http"
-	_ "net/http/pprof"
+	_ "net/http/pprof" // #nosec G108
+	"time"
 
 	"k8s.io/klog"
 )
 
 func StartDefaultPprofServer() {
-	if err := http.ListenAndServe(":6060", nil); err != nil {
+	server := &http.Server{
+		Addr:              ":6060",
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+	if err := server.ListenAndServe(); err != nil {
 		klog.Error(err, "failed to start the pprof server")
 	}
 }
