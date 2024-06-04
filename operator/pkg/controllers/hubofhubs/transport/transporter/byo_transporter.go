8473733947dfccf5aa537b19aeebcf85c6d5094a
myan <myan@redhat.com>
2024-06-04 10:01:37 +0800

    go lint
    Signed-off-by: myan <myan@redhat.com>

--- operator/pkg/controllers/hubofhubs/transport/transporter/byo_transporter.go
+++ operator/pkg/controllers/hubofhubs/transport/transporter/byo_transporter.go
@@ -2,15 +2,13 @@ package transporter
 
 import (
 	"context"
-	"encoding/base64"
-	"path/filepath"
 
 	"github.com/go-logr/logr"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/stolostron/multicluster-global-hub/operator/pkg/config"
 	"github.com/stolostron/multicluster-global-hub/pkg/transport"
 )
 
@@ -76,19 +74,5 @@ func (k *BYOTransporter) GenerateClusterTopic(clusterIdentity string) *transport
 }
 
 func (s *BYOTransporter) GetConnCredential(username string) (*transport.ConnCredential, error) {
-	kafkaSecret := &corev1.Secret{}
-	err := s.runtimeClient.Get(s.ctx, types.NamespacedName{
-		Name:      s.name,
-		Namespace: s.namespace,
-	}, kafkaSecret)
-	if err != nil {
-		return nil, err
-	}
-	return &transport.ConnCredential{
-		Identity:        string(kafkaSecret.Data[filepath.Join("bootstrap_server")]),
-		BootstrapServer: string(kafkaSecret.Data[filepath.Join("bootstrap_server")]),
-		CACert:          base64.StdEncoding.EncodeToString(kafkaSecret.Data[filepath.Join("ca.crt")]),
-		ClientCert:      base64.StdEncoding.EncodeToString(kafkaSecret.Data[filepath.Join("client.crt")]),
-		ClientKey:       base64.StdEncoding.EncodeToString(kafkaSecret.Data[filepath.Join("client.key")]),
-	}, nil
+	return config.GetConnFromGHTransportSecret(s.ctx, s.runtimeClient)
 }
