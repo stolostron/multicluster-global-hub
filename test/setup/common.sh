971fe235a98635747e584ff94a406245a7d569d9
myan <myan@redhat.com>
2024-06-10 07:05:17 +0000

    don't speci docker
    Signed-off-by: myan <myan@redhat.com>

--- test/setup/common.sh
+++ test/setup/common.sh
@@ -503,7 +503,7 @@ check_ginkgo() {
 install_docker() {
   sudo yum install -y yum-utils device-mapper-persistent-data lvm2
   sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  sudo yum install -y docker-ce-3:26.1.3-1.el8 docker-ce-cli-3:26.1.3-1.el8 containerd.io docker-compose-plugin
+  sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
   sleep 5
   sudo systemctl start docker
