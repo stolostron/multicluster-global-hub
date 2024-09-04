c188f59be22a9f5ddda7e77e35e299247a698428
Chunlin Yang <chuyang@redhat.com>
2024-09-04 23:12:26 +0800

    :memo: Use KRaft for BYO e2e (#1085)
    * Use KRaft for BYO e2e
    
    Signed-off-by: clyang82 <chuyang@redhat.com>
    
    * Install Kafka in kafka namespace
    
    Signed-off-by: clyang82 <chuyang@redhat.com>
    
    ---------
    
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- test/script/e2e_kafka.sh
+++ test/script/e2e_kafka.sh
@@ -16,65 +16,47 @@ echo -e "\r${BOLD_GREEN}[ START - $(date +"%T") ] Install Kafka $NC"
 # check the transport secret
 transport_secret=${TRANSPORT_SECRET_NAME:-"multicluster-global-hub-transport"}
 target_namespace=${TARGET_NAMESPACE:-"multicluster-global-hub"}
+kafka_namespace=${KAFKA_NAMESPACE:-"kafka"}
 if kubectl get secret "$transport_secret" -n "$target_namespace" --kubeconfig "$SECRET_KUBECONFIG"; then
   echo "transport_secret $transport_secret already exists in $target_namespace namespace"
   exit 0
 fi
 
 # create all the resource in cluster KUBECONFIG
-kubectl create namespace "$target_namespace" --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace "$kafka_namespace" --dry-run=client -o yaml | kubectl apply -f -
 
 # deploy kafka operator
-kubectl -n $target_namespace create -f "https://strimzi.io/install/latest?namespace=$target_namespace"
-retry "(kubectl get pods -n $target_namespace -l name=strimzi-cluster-operator | grep Running)" 60
+kubectl -n $kafka_namespace create -f "https://strimzi.io/install/latest?namespace=$kafka_namespace"
+retry "(kubectl get pods -n $kafka_namespace -l name=strimzi-cluster-operator | grep Running)" 60
 
 echo "Kafka operator is ready"
 
-# deploy kafka cluster
-kubectl apply -k "$TEST_DIR"/manifest/kafka/kafka-cluster -n "$target_namespace"
-
-# patch the nodeport IP to the broker certificate Subject Alternative Name(SAN)
-# or node_port_host=$(kubectl -n "$target_namespace" get kafka.kafka.strimzi.io/kafka -o jsonpath='{.status.listeners[1].addresses[0].host}')
 node_port_host=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}' | sed -e 's#^https\?://##' -e 's/:.*//')
-kubectl -n "$target_namespace" patch kafka.kafka.strimzi.io/kafka --type json -p '[
-  {
-    "op": "replace",
-    "path": "/spec/kafka/listeners/1/configuration",
-    "value": {
-      "bootstrap": {
-        "nodePort": 30095
-      },
-      "brokers": [
-        {
-          "broker": 0,
-          "advertisedHost": "'"$node_port_host"'", 
-        }
-      ]
-    }
-  }
-]'
+sed -i -e "s;NODE_PORT_HOST;$node_port_host;" "$TEST_DIR"/manifest/kafka/kafka-cluster/kafka-cluster.yaml
+# deploy kafka cluster
+kubectl apply -k "$TEST_DIR"/manifest/kafka/kafka-cluster -n "$kafka_namespace"
 
-wait_cmd "kubectl get kafka kafka -n $target_namespace -o jsonpath='{.status.listeners[1]}' | grep bootstrapServers"
+wait_cmd "kubectl get kafka kafka -n $kafka_namespace -o jsonpath='{.status.listeners[1]}' | grep bootstrapServers"
 
 # kafka
-wait_cmd "kubectl get kafkatopic gh-spec -n multicluster-global-hub | grep -C 1 True"
-wait_cmd "kubectl get kafkatopic gh-status.hub1 -n multicluster-global-hub | grep -C 1 True"
-wait_cmd "kubectl get kafkatopic gh-status.hub2 -n multicluster-global-hub | grep -C 1 True"
-wait_cmd "kubectl get kafkauser global-hub-kafka-user -n multicluster-global-hub | grep -C 1 True"
+wait_cmd "kubectl get kafkatopic gh-spec -n $kafka_namespace | grep -C 1 True"
+wait_cmd "kubectl get kafkatopic gh-status.hub1 -n $kafka_namespace | grep -C 1 True"
+wait_cmd "kubectl get kafkatopic gh-status.hub2 -n $kafka_namespace | grep -C 1 True"
+wait_cmd "kubectl get kafkauser global-hub-kafka-user -n $kafka_namespace | grep -C 1 True"
 echo "Kafka topic/user are ready"
 
 # Note: skip to create the transport secret, trying to use the the internal multi users and topics for managed hubs
 
 # BYO: 1. create the topics; 2. create the user; 3. create the transport secret
 byo_user=global-hub-byo-user
-wait_cmd "kubectl get kafkauser $byo_user -n $target_namespace | grep -C 1 True"
+wait_cmd "kubectl get kafkauser $byo_user -n $kafka_namespace | grep -C 1 True"
 
 # generate transport secret
-wait_cmd "kubectl get kafka kafka -n $target_namespace -o jsonpath='{.status.listeners[1]}' | grep bootstrapServers"
-bootstrap_server=$(kubectl get kafka kafka -n "$target_namespace" -o jsonpath='{.status.listeners[1].bootstrapServers}')
-kubectl get kafka kafka -n "$target_namespace" -o jsonpath='{.status.listeners[1].certificates[0]}' >"$CURRENT_DIR"/config/kafka-ca-cert.pem
-kubectl get secret $byo_user -n "$target_namespace" -o jsonpath='{.data.user\.crt}' | base64 -d >"$CURRENT_DIR"/config/kafka-client-cert.pem
-kubectl get secret $byo_user -n "$target_namespace" -o jsonpath='{.data.user\.key}' | base64 -d >"$CURRENT_DIR"/config/kafka-client-key.pem
+wait_cmd "kubectl get kafka kafka -n $kafka_namespace -o jsonpath='{.status.listeners[1]}' | grep bootstrapServers"
+bootstrap_server=$(kubectl get kafka kafka -n "$kafka_namespace" -o jsonpath='{.status.listeners[1].bootstrapServers}')
+kubectl get kafka kafka -n "$kafka_namespace" -o jsonpath='{.status.listeners[1].certificates[0]}' >"$CURRENT_DIR"/config/kafka-ca-cert.pem
+kubectl get secret $byo_user -n "$kafka_namespace" -o jsonpath='{.data.user\.crt}' | base64 -d >"$CURRENT_DIR"/config/kafka-client-cert.pem
+kubectl get secret $byo_user -n "$kafka_namespace" -o jsonpath='{.data.user\.key}' | base64 -d >"$CURRENT_DIR"/config/kafka-client-key.pem
 
 # generate the secret in the target cluster: SECRET_KUBECONFIG
 
