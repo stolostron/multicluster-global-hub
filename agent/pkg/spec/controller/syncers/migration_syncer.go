993b04cac5244a3d615d5c28913604f2e150926d
morvencao <lcao@redhat.com>
2024-09-24 07:31:41 +0000

    fix agent migration syncer.
    Signed-off-by: morvencao <lcao@redhat.com>

--- agent/pkg/spec/controller/syncers/migration_syncer.go
+++ agent/pkg/spec/controller/syncers/migration_syncer.go
@@ -8,6 +8,7 @@ import (
 	klusterletv1alpha1 "github.com/stolostron/cluster-lifecycle-api/klusterletconfig/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
 	operatorv1 "open-cluster-management.io/api/operator/v1"
@@ -15,6 +16,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	bundleevent "github.com/stolostron/multicluster-global-hub/pkg/bundle/event"
+)
+
+const (
+	bootstrapSecretBackupSuffix = "-backup"
 )
 
 type managedClusterMigrationSyncer struct {
@@ -42,8 +47,8 @@ func (syncer *managedClusterMigrationSyncer) Sync(payload []byte) error {
 		return err
 	}
 
+	// create or update bootstrap secret
 	bootstrapSecret := managedClusterMigrationEvent.BootstrapSecret
-
 	foundBootstrapSecret := &corev1.Secret{}
 	if err := syncer.client.Get(syncer.context,
 		types.NamespacedName{
@@ -58,9 +63,55 @@ func (syncer *managedClusterMigrationSyncer) Sync(payload []byte) error {
 		} else {
 			return err
 		}
+	} else {
+		// update the bootstrap secret if it already exists
+		syncer.log.Info("updating bootstrap secret", "bootstrap secret", bootstrapSecret)
+		if err := syncer.client.Update(syncer.context, bootstrapSecret); err != nil {
+			return err
+		}
 	}
 
+	// create or update boostrap secret backup
+	bootstrapSecretBackup := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      bootstrapSecret.Name + bootstrapSecretBackupSuffix,
+			Namespace: bootstrapSecret.Namespace,
+		},
+		Data: bootstrapSecret.Data,
+	}
+	foundBootstrapSecretBackup := &corev1.Secret{}
+	if err := syncer.client.Get(syncer.context,
+		types.NamespacedName{
+			Name:      bootstrapSecretBackup.Name,
+			Namespace: bootstrapSecretBackup.Namespace,
+		}, foundBootstrapSecretBackup); err != nil {
+		if apierrors.IsNotFound(err) {
+			syncer.log.Info("creating bootstrap backup secret", "bootstrap backup secret", bootstrapSecretBackup)
+			if err := syncer.client.Create(syncer.context, bootstrapSecretBackup); err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	} else {
+		// update the bootstrap backup secret if it already exists
+		syncer.log.Info("updating bootstrap backup secret", "bootstrap backup secret", bootstrapSecretBackup)
+		if err := syncer.client.Update(syncer.context, bootstrapSecret); err != nil {
+			return err
+		}
+	}
+
+	// create klusterlet config if it does not exist
 	klusterletConfig := managedClusterMigrationEvent.KlusterletConfig
+	// set the bootstrap kubeconfig secrets in klusterlet config
+	klusterletConfig.Spec.BootstrapKubeConfigs.LocalSecrets.KubeConfigSecrets = []operatorv1.KubeConfigSecret{
+		{
+			Name: bootstrapSecret.Name,
+		},
+		{
+			Name: bootstrapSecretBackup.Name,
+		},
+	}
 	foundKlusterletConfig := &klusterletv1alpha1.KlusterletConfig{}
 	if err := syncer.client.Get(syncer.context,
 		types.NamespacedName{
@@ -89,6 +140,9 @@ func (syncer *managedClusterMigrationSyncer) Sync(payload []byte) error {
 		if annotations == nil {
 			annotations = make(map[string]string)
 		}
+		if annotations["agent.open-cluster-management.io/klusterlet-config"] == klusterletConfig.Name {
+			continue
+		}
 		annotations["agent.open-cluster-management.io/klusterlet-config"] = klusterletConfig.Name
 		mcl.SetAnnotations(annotations)
 		if err := syncer.client.Update(syncer.context, mcl); err != nil {
@@ -105,6 +159,30 @@ func (syncer *managedClusterMigrationSyncer) patchClusterManager() error {
 	if err := syncer.client.Get(syncer.context, namespacedName, clusterManager); err != nil {
 		return err
 	}
+
+	// check if ManagedClusterAutoApproval feature gate and auto approve user are enabled
+	if clusterManager.Spec.RegistrationConfiguration != nil {
+		featureGateEnabled := false
+		for _, featureGate := range clusterManager.Spec.RegistrationConfiguration.FeatureGates {
+			if featureGate.Feature == "ManagedClusterAutoApproval" {
+				if featureGate.Mode == operatorv1.FeatureGateModeTypeEnable {
+					featureGateEnabled = true
+					break
+				}
+			}
+		}
+		autoApproveUserEnabled := false
+		for _, autoApproveUser := range clusterManager.Spec.RegistrationConfiguration.AutoApproveUsers {
+			if autoApproveUser == "system:serviceaccount:open-cluster-management:agent-registration-bootstrap" {
+				autoApproveUserEnabled = true
+				break
+			}
+		}
+		if featureGateEnabled && autoApproveUserEnabled {
+			return nil
+		}
+	}
+
 	patchObject := []byte(`
 apiVersion: operator.open-cluster-management.io/v1
 kind: ClusterManager
