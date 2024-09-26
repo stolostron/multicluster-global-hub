fae6376afd47ce08143e6a20ce254b824406da65
morvencao <lcao@redhat.com>
2024-09-26 10:26:10 +0000

    cleanup migrated managedclusters.
    Signed-off-by: morvencao <lcao@redhat.com>

--- agent/pkg/spec/controller/syncers/migration_from_syncer.go
+++ agent/pkg/spec/controller/syncers/migration_from_syncer.go
@@ -3,13 +3,16 @@ package syncers
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	"github.com/go-logr/logr"
 	klusterletv1alpha1 "github.com/stolostron/cluster-lifecycle-api/klusterletconfig/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
 	operatorv1 "open-cluster-management.io/api/operator/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -90,7 +93,7 @@ func (s *managedClusterMigrationFromSyncer) Sync(ctx context.Context, payload []
 	} else {
 		// update the bootstrap backup secret if it already exists
 		s.log.Info("updating bootstrap backup secret", "bootstrap backup secret", bootstrapSecretBackup)
-		if err := s.client.Update(ctx, bootstrapSecret); err != nil {
+		if err := s.client.Update(ctx, bootstrapSecretBackup); err != nil {
 			return err
 		}
 	}
@@ -144,5 +147,36 @@ func (s *managedClusterMigrationFromSyncer) Sync(ctx context.Context, payload []
 		}
 	}
 
+	// check managed cluster available unknown status and detach the managed cluster in new go routine
+	if err := s.detachManagedClusters(ctx, managedClusters); err != nil {
+		s.log.Error(err, "failed to detach managed clusters")
+	}
+
 	return nil
+}
+
+func (s *managedClusterMigrationFromSyncer) detachManagedClusters(ctx context.Context, managedClusters []string) error {
+	return wait.PollUntilContextCancel(ctx, 2*time.Second, true, func(ctx context.Context) (bool, error) {
+		for _, managedCluster := range managedClusters {
+			mcl := &clusterv1.ManagedCluster{}
+			if err := s.client.Get(ctx, types.NamespacedName{
+				Name: managedCluster,
+			}, mcl); err != nil {
+				if apierrors.IsNotFound(err) {
+					continue
+				} else {
+					return false, err
+				}
+			}
+			if meta.IsStatusConditionPresentAndEqual(mcl.Status.Conditions,
+				clusterv1.ManagedClusterConditionAvailable, metav1.ConditionUnknown) {
+				if err := s.client.Delete(ctx, mcl); err != nil {
+					return false, err
+				}
+			} else {
+				return false, nil
+			}
+		}
+		return true, nil
+	})
 }
