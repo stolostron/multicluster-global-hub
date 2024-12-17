7bb05cf112ea59eb896a394ffa8c4628d3e52457
clyang82 <chuyang@redhat.com>
2024-12-17 15:02:01 +0800

    Bump go version to 1.23.4
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- pkg/transport/consumer/sarama_consumer_test.go
+++ pkg/transport/consumer/sarama_consumer_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"testing"
 
-	"github.com/Shopify/sarama"
+	"github.com/IBM/sarama"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
