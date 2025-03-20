ae9c46525cdf06036059ec976b5c324abbc5e0b8
clyang82 <chuyang@redhat.com>
2025-03-20 11:23:37 +0800

    Generate storage secret correctly
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- test/script/e2e_postgres.sh
+++ test/script/e2e_postgres.sh
@@ -28,17 +28,6 @@ retry "kubectl apply --server-side -k $TEST_DIR/manifest/postgres/postgres-opera
 # step3: deploy  postgres cluster
 retry "kubectl apply -k $TEST_DIR/manifest/postgres/postgres-cluster && (kubectl get secret $ps_user -n $pg_ns)"
 
-# step4: generate storage secret 
-databaseURI=$(kubectl get secrets -n "${pg_ns}" "${ps_user}" -o go-template='{{index (.data) "uri" | base64decode}}')
-kubectl get secret $pg_cert -n $pg_ns -o jsonpath='{.data.ca\.crt}' |base64 -d > "$CONFIG_DIR/postgres-cluster-ca.crt"
-
-kubectl create namespace "$target_namespace" --dry-run=client -o yaml | kubectl apply -f -
-kubectl create secret generic "$storage_secret" -n "$target_namespace" \
-    --from-literal=database_uri="${databaseURI}?sslmode=verify-ca" \
-    --from-file=ca.crt="$CONFIG_DIR/postgres-cluster-ca.crt"
-
-echo "storage secret is ready in $target_namespace namespace!"
-
 # expose the postgres service as NodePort
 kubectl patch postgrescluster hoh -n $pg_ns -p '{"spec":{"service":{"type":"NodePort", "nodePort": 32432}}}'  --type merge
 
@@ -48,10 +37,26 @@ for sts in ${stss}; do
 done
 
 kubectl delete pod -n $pg_ns --all --ignore-not-found=true 2>/dev/null  
-echo "Postgres is pathed!"
 
 # postgres
 wait_cmd "kubectl get pods -l postgres-operator.crunchydata.com/instance-set=pgha1 -n $pg_ns | grep Running"
 kubectl wait --for=condition=ready pod -l postgres-operator.crunchydata.com/instance-set=pgha1 -n $pg_ns--timeout=100s
+echo "Postgres cluster is ready!"
 
-echo -e "\r${BOLD_GREEN}[ END ] Install Postgres ${NC} $(($(date +%s) - start_time)) seconds"
+database_uri=$(kubectl get secrets -n "${pg_ns}" "${ps_user}" -o go-template='{{index (.data) "uri" | base64decode}}')
+kubectl get secret $pg_cert -n $pg_ns -o jsonpath='{.data.ca\.crt}' |base64 -d > "$CONFIG_DIR/postgres-cluster-ca.crt"
+
+# covert the database uri into external uri
+external_host=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}' | sed -e 's#^https\?://##' -e 's/:.*//')
+external_port=32432
+database_uri=$(echo "${database_uri}" | sed "s|@[^/]*|@$external_host:$external_port|")
+
+# step4: generate storage secret
+kubectl create namespace "$target_namespace" --dry-run=client -o yaml | kubectl apply -f -
+kubectl create secret generic "$storage_secret" -n "$target_namespace" \
+    --from-literal=database_uri="${database_uri}?sslmode=verify-ca" \
+    --from-file=ca.crt="$CONFIG_DIR/postgres-cluster-ca.crt"
+
+echo "Storage secret is ready in $target_namespace namespace!"
+
+echo -e "\r${BOLD_GREEN}[ END - $(date +"%T") ] Install Postgres ${NC} $(($(date +%s) - start_time)) seconds"
