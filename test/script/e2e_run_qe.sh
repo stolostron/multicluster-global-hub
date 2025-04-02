16d4801525dea60fb8467722ffa9c6c9ad2c7b17
clyang82 <chuyang@redhat.com>
2025-04-02 16:30:48 +0800

    Run QE cases on Monday
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- test/script/e2e_run_qe.sh
+++ test/script/e2e_run_qe.sh
@@ -2,18 +2,28 @@
 
 set -euo pipefail
 
-CURRENT_DIR=$(
-  cd "$(dirname "$0")" || exit
-  pwd
-)
-source "$CURRENT_DIR/util.sh"
 
-CONFIG_DIR=$CURRENT_DIR/config
-GH_KUBECONFIG="${CONFIG_DIR}/global-hub"
-MH1_KUBECONFIG="${CONFIG_DIR}/hub1"
+# Get day of week (1-7, where 1 is Monday)
+NOW=$(date +%u)
 
-docker pull quay.io/hchenxa/acmqe-hoh-e2e
-docker run --name qe-test -d quay.io/hchenxa/acmqe-hoh-e2e  tail -f /dev/null
-docker cp qe-test:/e2e.test ./
+# Run only on Monday (2)
+if [ "$NOW" = "1" ] then
+  CURRENT_DIR=$(
+    cd "$(dirname "$0")" || exit
+    pwd
+  )
+  source "$CURRENT_DIR/util.sh"
 
-SERVICE_TYPE=NODE_PORT KUBECONFIG=$GH_KUBECONFIG SPOKE_KUBECONFIG=$MH1_KUBECONFIG ./e2e.test --ginkgo.v --ginkgo.label-filter='e2e'
+  CONFIG_DIR=$CURRENT_DIR/config
+  GH_KUBECONFIG="${CONFIG_DIR}/global-hub"
+  MH1_KUBECONFIG="${CONFIG_DIR}/hub1"
+
+  docker pull quay.io/hchenxa/acmqe-hoh-e2e
+  docker run --name qe-test -d quay.io/hchenxa/acmqe-hoh-e2e  tail -f /dev/null
+  docker cp qe-test:/e2e.test ./
+
+  SERVICE_TYPE=NODE_PORT KUBECONFIG=$GH_KUBECONFIG SPOKE_KUBECONFIG=$MH1_KUBECONFIG ./e2e.test --ginkgo.v --ginkgo.label-filter='e2e'
+
+else
+    echo "Skipping QE tests (not scheduled day)"
+fi
