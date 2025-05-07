227f72077ea33fa17d0c89ee846e11bfbee445ef
root <daliu@redhat.com>
2025-05-07 06:03:57 +0000

    move-inventory: cluster
    Signed-off-by: root <daliu@redhat.com>

--- manager/pkg/status/handlers/managedcluster/managedcluster_handler_test.go
+++ manager/pkg/status/handlers/managedcluster/managedcluster_handler_test.go
@@ -8,8 +8,6 @@ import (
 	kessel "github.com/project-kessel/inventory-api/api/kessel/inventory/v1beta1/resources"
 	"github.com/project-kessel/inventory-client-go/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes/scheme"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
 
 	"github.com/stolostron/multicluster-global-hub/pkg/constants"
@@ -17,118 +15,6 @@ import (
 	"github.com/stolostron/multicluster-global-hub/pkg/logger"
 	"github.com/stolostron/multicluster-global-hub/pkg/transport"
 )
-
-func TestManagedClusterHandler_postToInventoryApi(t *testing.T) {
-	type args struct {
-		ctx            context.Context
-		createClusters []clusterv1.ManagedCluster
-		updateClusters []clusterv1.ManagedCluster
-		deleteClusters []models.ResourceVersion
-		leafHubName    string
-		mchVersion     string
-	}
-
-	tests := []struct {
-		name                       string
-		args                       args
-		wantCreate                 int
-		wantUpdate                 int
-		wantDelete                 int
-		requesterShouldReturnError bool
-		initObjects                []runtime.Object
-	}{
-		{
-			name: "successful create/update/delete",
-			args: args{
-				ctx: context.TODO(),
-				createClusters: []clusterv1.ManagedCluster{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name: "cluster1",
-						},
-						Status: clusterv1.ManagedClusterStatus{
-							ClusterClaims: []clusterv1.ManagedClusterClaim{
-								{Name: constants.ClusterIdClaimName, Value: "test-id-1"},
-							},
-						},
-					},
-				},
-				updateClusters: []clusterv1.ManagedCluster{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name: "cluster2",
-						},
-						Status: clusterv1.ManagedClusterStatus{
-							ClusterClaims: []clusterv1.ManagedClusterClaim{
-								{Name: constants.ClusterIdClaimName, Value: "test-id-2"},
-							},
-						},
-					},
-				},
-				deleteClusters: []models.ResourceVersion{
-					{
-						Key:  "cluster3",
-						Name: "cluster3",
-					},
-				},
-				leafHubName: "hub1",
-				mchVersion:  "2.8.0",
-			},
-			wantCreate:                 1,
-			wantUpdate:                 1,
-			wantDelete:                 1,
-			requesterShouldReturnError: false,
-		},
-		{
-			name: "empty clusters",
-			args: args{
-				ctx:            context.TODO(),
-				createClusters: []clusterv1.ManagedCluster{},
-				updateClusters: []clusterv1.ManagedCluster{},
-				deleteClusters: []models.ResourceVersion{},
-				leafHubName:    "hub1",
-				mchVersion:     "2.8.0",
-			},
-			wantCreate:                 0,
-			wantUpdate:                 0,
-			wantDelete:                 0,
-			requesterShouldReturnError: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			_ = clusterv1.AddToScheme(scheme.Scheme)
-
-			h := &managedClusterHandler{
-				log: logger.ZapLogger("test"),
-			}
-			fakeRequester := &FakeRequester{
-				HttpClient: &v1beta1.InventoryHttpClient{
-					K8sClusterService: &FakeKesselK8SClusterServiceHTTPClientImpl{},
-				},
-			}
-			gotCreate, gotUpdate, gotDelete := h.postToInventoryApi(
-				tt.args.ctx,
-				fakeRequester,
-				tt.args.createClusters,
-				tt.args.updateClusters,
-				tt.args.deleteClusters,
-				tt.args.leafHubName,
-			)
-
-			if len(gotCreate) != tt.wantCreate {
-				t.Errorf("postToInventoryApi() gotCreate = %v, want %v", len(gotCreate), tt.wantCreate)
-			}
-			if len(gotUpdate) != tt.wantUpdate {
-				t.Errorf("postToInventoryApi() gotUpdate = %v, want %v", len(gotUpdate), tt.wantUpdate)
-			}
-			if len(gotDelete) != tt.wantDelete {
-				t.Errorf("postToInventoryApi() gotDelete = %v, want %v", len(gotDelete), tt.wantDelete)
-			}
-		})
-	}
-}
 
 func TestGetK8SCluster(t *testing.T) {
 	tests := []struct {
@@ -267,7 +153,342 @@ func TestGetK8SCluster(t *testing.T) {
 	}
 }
 
-// FakeRequester is a mock implementation of the Requester interface.
+func TestPostToInventoryApi(t *testing.T) {
+	tests := []struct {
+		name           string
+		createClusters []clusterv1.ManagedCluster
+		updateClusters []clusterv1.ManagedCluster
+		deleteClusters []models.ResourceVersion
+		leafHubName    string
+		clusterInfo    models.ClusterInfo
+	}{
+		{
+			name: "create clusters",
+			createClusters: []clusterv1.ManagedCluster{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-cluster-1",
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						ClusterClaims: []clusterv1.ManagedClusterClaim{
+							{Name: constants.ClusterIdClaimName, Value: "test-id-1"},
+						},
+					},
+				},
+			},
+			leafHubName: "leaf-hub-1",
+			clusterInfo: models.ClusterInfo{
+				ConsoleURL: "https://console.example.com",
+				MchVersion: "2.8.0",
+			},
+		},
+		{
+			name: "update clusters",
+			updateClusters: []clusterv1.ManagedCluster{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-cluster-2",
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						ClusterClaims: []clusterv1.ManagedClusterClaim{
+							{Name: constants.ClusterIdClaimName, Value: "test-id-2"},
+						},
+					},
+				},
+			},
+			leafHubName: "leaf-hub-2",
+			clusterInfo: models.ClusterInfo{},
+		},
+		{
+			name: "delete clusters",
+			deleteClusters: []models.ResourceVersion{
+				{
+					Key:  "test-id-3",
+					Name: "test-cluster-3",
+				},
+			},
+			leafHubName: "leaf-hub-3",
+			clusterInfo: models.ClusterInfo{},
+		},
+		{
+			name:           "no clusters",
+			createClusters: []clusterv1.ManagedCluster{},
+			updateClusters: []clusterv1.ManagedCluster{},
+			deleteClusters: []models.ResourceVersion{},
+			leafHubName:    "leaf-hub-4",
+			clusterInfo:    models.ClusterInfo{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create mock implementations
+			fakeClient := &FakeKesselK8SClusterServiceHTTPClientImpl{}
+			fakeRequester := &FakeRequester{
+				HttpClient: &v1beta1.InventoryHttpClient{
+					K8sClusterService: fakeClient,
+				},
+			}
+
+			h := &managedClusterHandler{
+				log: logger.ZapLogger("test"),
+			}
+
+			// Execute function under test
+			h.postToInventoryApi(
+				context.Background(),
+				fakeRequester,
+				tt.clusterInfo,
+				tt.createClusters,
+				tt.updateClusters,
+				tt.deleteClusters,
+				tt.leafHubName,
+			)
+		})
+	}
+} // FakeRequester is a mock implementation of the Requester interface.
+
+func TestGenerateCreateUpdateDeleteClusters(t *testing.T) {
+	handler := &managedClusterHandler{
+		log: logger.ZapLogger("test"),
+	}
+
+	tests := []struct {
+		name                        string
+		data                        []clusterv1.ManagedCluster
+		leafHubName                 string
+		clusterIdToVersionMapFromDB map[string]models.ResourceVersion
+		wantCreate                  int
+		wantUpdate                  int
+		wantDelete                  int
+	}{
+		{
+			name: "new clusters should be created",
+			data: []clusterv1.ManagedCluster{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "cluster1",
+						ResourceVersion: "1",
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						ClusterClaims: []clusterv1.ManagedClusterClaim{
+							{Name: constants.ClusterIdClaimName, Value: "id1"},
+						},
+					},
+				},
+			},
+			leafHubName:                 "hub1",
+			clusterIdToVersionMapFromDB: map[string]models.ResourceVersion{},
+			wantCreate:                  1,
+			wantUpdate:                  0,
+			wantDelete:                  0,
+		},
+		{
+			name: "existing clusters should be updated",
+			data: []clusterv1.ManagedCluster{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "cluster1",
+						ResourceVersion: "2",
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						ClusterClaims: []clusterv1.ManagedClusterClaim{
+							{Name: constants.ClusterIdClaimName, Value: "id1"},
+						},
+					},
+				},
+			},
+			leafHubName: "hub1",
+			clusterIdToVersionMapFromDB: map[string]models.ResourceVersion{
+				"id1": {
+					Key:             "id1",
+					Name:            "cluster1",
+					ResourceVersion: "1",
+				},
+			},
+			wantCreate: 0,
+			wantUpdate: 1,
+			wantDelete: 0,
+		},
+		{
+			name:        "missing clusters should be deleted",
+			data:        []clusterv1.ManagedCluster{},
+			leafHubName: "hub1",
+			clusterIdToVersionMapFromDB: map[string]models.ResourceVersion{
+				"id1": {
+					Key:             "id1",
+					Name:            "cluster1",
+					ResourceVersion: "1",
+				},
+			},
+			wantCreate: 0,
+			wantUpdate: 0,
+			wantDelete: 1,
+		},
+		{
+			name: "skip clusters without ID claim",
+			data: []clusterv1.ManagedCluster{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "cluster1",
+						ResourceVersion: "1",
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						ClusterClaims: []clusterv1.ManagedClusterClaim{},
+					},
+				},
+			},
+			leafHubName:                 "hub1",
+			clusterIdToVersionMapFromDB: map[string]models.ResourceVersion{},
+			wantCreate:                  0,
+			wantUpdate:                  0,
+			wantDelete:                  0,
+		},
+		{
+			name: "no changes needed",
+			data: []clusterv1.ManagedCluster{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "cluster1",
+						ResourceVersion: "1",
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						ClusterClaims: []clusterv1.ManagedClusterClaim{
+							{Name: constants.ClusterIdClaimName, Value: "id1"},
+						},
+					},
+				},
+			},
+			leafHubName: "hub1",
+			clusterIdToVersionMapFromDB: map[string]models.ResourceVersion{
+				"id1": {
+					Key:             "id1",
+					Name:            "cluster1",
+					ResourceVersion: "1",
+				},
+			},
+			wantCreate: 0,
+			wantUpdate: 0,
+			wantDelete: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			createClusters, updateClusters, deleteClusters := handler.generateCreateUpdateDeleteClusters(
+				tt.data,
+				tt.leafHubName,
+				tt.clusterIdToVersionMapFromDB,
+			)
+
+			if len(createClusters) != tt.wantCreate {
+				t.Errorf("got %d clusters to create, want %d", len(createClusters), tt.wantCreate)
+			}
+			if len(updateClusters) != tt.wantUpdate {
+				t.Errorf("got %d clusters to update, want %d", len(updateClusters), tt.wantUpdate)
+			}
+			if len(deleteClusters) != tt.wantDelete {
+				t.Errorf("got %d clusters to delete, want %d", len(deleteClusters), tt.wantDelete)
+			}
+		})
+	}
+}
+
+func TestSyncInventory(t *testing.T) {
+	tests := []struct {
+		name                        string
+		data                        []clusterv1.ManagedCluster
+		leafHubName                 string
+		clusterIdToVersionMapFromDB map[string]models.ResourceVersion
+		requesterNil                bool
+		wantErr                     bool
+	}{
+		{
+			name: "successful sync with changes",
+			data: []clusterv1.ManagedCluster{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "cluster1",
+						ResourceVersion: "2",
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						ClusterClaims: []clusterv1.ManagedClusterClaim{
+							{Name: constants.ClusterIdClaimName, Value: "id1"},
+						},
+					},
+				},
+			},
+			leafHubName: "hub1",
+			clusterIdToVersionMapFromDB: map[string]models.ResourceVersion{
+				"id2": {
+					Key:             "id2",
+					Name:            "cluster2",
+					ResourceVersion: "1",
+				},
+			},
+			requesterNil: false,
+			wantErr:      false,
+		},
+		{
+			name:                        "no changes needed",
+			data:                        []clusterv1.ManagedCluster{},
+			leafHubName:                 "hub1",
+			clusterIdToVersionMapFromDB: map[string]models.ResourceVersion{},
+			requesterNil:                false,
+			wantErr:                     false,
+		},
+		{
+			name: "error when requester is nil",
+			data: []clusterv1.ManagedCluster{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "cluster1",
+						ResourceVersion: "1",
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						ClusterClaims: []clusterv1.ManagedClusterClaim{
+							{Name: constants.ClusterIdClaimName, Value: "id1"},
+						},
+					},
+				},
+			},
+			leafHubName:                 "hub1",
+			clusterIdToVersionMapFromDB: map[string]models.ResourceVersion{},
+			requesterNil:                true,
+			wantErr:                     true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create handler with mock components
+			h := &managedClusterHandler{
+				log: logger.ZapLogger("test"),
+			}
+
+			if !tt.requesterNil {
+				fakeClient := &FakeKesselK8SClusterServiceHTTPClientImpl{}
+				h.requester = &FakeRequester{
+					HttpClient: &v1beta1.InventoryHttpClient{
+						K8sClusterService: fakeClient,
+					},
+				}
+			}
+
+			err := h.syncInventory(
+				context.Background(),
+				tt.data,
+				tt.leafHubName,
+				tt.clusterIdToVersionMapFromDB,
+			)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("syncInventory() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
 type FakeRequester struct {
 	HttpClient *v1beta1.InventoryHttpClient
 }
