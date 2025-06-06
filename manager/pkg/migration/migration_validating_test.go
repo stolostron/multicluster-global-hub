f25d07b9ed9db2d71eaf9abca6b461ede6747706
Meng Yan <myan@redhat.com>
2025-06-06 09:50:41 +0800

    validate configmap (#1731)
    Signed-off-by: Meng Yan <myan@redhat.com>

--- manager/pkg/migration/migration_validating_test.go
+++ manager/pkg/migration/migration_validating_test.go
@@ -3,40 +3,37 @@ package migration
 import (
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-// go test -run ^TestIsValidResource$ github.com/stolostron/multicluster-global-hub/manager/pkg/migration -v
+// go test -run ^TestIsValidResource$ ./manager/pkg/migration -v
 func TestIsValidResource(t *testing.T) {
 	tests := []struct {
 		name     string
 		input    string
-		wantErr  bool
 		errorMsg string
 	}{
-		{"valid configmap", "configmap/default/my-config", false, ""},
-		{"valid secret with wildcard", "secret/ns1/*", true, "invalid name"},
-		{"invalid format", "configmap/default", true, "invalid format"},
-		{"unsupported kind", "pod/ns1/name1", true, "unsupported kind"},
-		{"invalid namespace", "configmap/Inv@lid/ns", true, "invalid namespace"},
-		{"invalid name", "secret/default/Invalid*", true, "invalid name"},
-		{"invalid wildcard placement", "secret/ns1/na*me", true, "invalid name"},
-		{"empty name with wildcard char", "secret/ns1/*x", true, "invalid name"},
+		{"valid configmap within dot", "configmap/default/kube-root-ca.crt", ""},
+		{"invalid configmap prefix dot", "configmap/default/.kube-root-ca.crt ", "invalid name: .kube-root-ca.crt"},
+		{"valid configmap", "configmap/default/my-config", ""},
+		{"valid secret with wildcard", "secret/ns1/*", "invalid name: *"},
+		{"invalid format", "configmap/default", "invalid format (must be kind/namespace/name): configmap/default"},
+		{"unsupported kind", "pod/ns1/name1", "unsupported kind: pod"},
+		{"invalid namespace", "configmap/Inv@lid/ns", "invalid namespace: Inv@lid"},
+		{"invalid name", "secret/default/Invalid*", "invalid name: Invalid*"},
+		{"invalid wildcard placement", "secret/ns1/na*me", "invalid name: na*me"},
+		{"empty name with wildcard char", "secret/ns1/*x", "invalid name: *x"},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := IsValidResource(tt.input)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("expected error=%v, got error=%v", tt.wantErr, err)
+			actualErrMessage := ""
+			if err != nil {
+				actualErrMessage = err.Error()
 			}
-			if tt.wantErr && err != nil && !contains(err.Error(), tt.errorMsg) {
-				t.Errorf("expected error message to contain '%s', got '%s'", tt.errorMsg, err.Error())
-			}
+			require.Equal(t, strings.TrimSpace(tt.errorMsg), strings.TrimSpace(actualErrMessage), tt.name)
 		})
 	}
-}
-
-// helper for substring match
-func contains(s, substr string) bool {
-	return strings.Contains(s, substr)
 }
