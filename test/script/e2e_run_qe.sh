3884f8a9e4ffe611486d23fb73a4ccc8226f381c
DangPeng Liu <daliu@redhat.com>
2025-06-19 10:21:26 +0800

    fix qe  e2e (#1757)
    Signed-off-by: root <daliu@redhat.com>

--- test/script/e2e_run_qe.sh
+++ test/script/e2e_run_qe.sh
@@ -12,8 +12,8 @@ CONFIG_DIR=$CURRENT_DIR/config
 GH_KUBECONFIG="${CONFIG_DIR}/global-hub"
 MH1_KUBECONFIG="${CONFIG_DIR}/hub1"
 
-docker pull quay.io/hchenxa/acmqe-hoh-e2e
-docker run --name qe-test -d quay.io/hchenxa/acmqe-hoh-e2e  tail -f /dev/null
+docker pull quay.io/hchenxa/acmqe-hoh-e2e:v1.5
+docker run --name qe-test -d quay.io/hchenxa/acmqe-hoh-e2e:v1.5  tail -f /dev/null
 docker cp qe-test:/e2e.test ./
 
 SERVICE_TYPE=NODE_PORT KUBECONFIG=$GH_KUBECONFIG SPOKE_KUBECONFIG=$MH1_KUBECONFIG ./e2e.test --ginkgo.v --ginkgo.label-filter='e2e && !migration'
