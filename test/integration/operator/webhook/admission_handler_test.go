18d332e9594a5c2698e482b58595f798a092dcd1
DangPeng Liu <daliu@redhat.com>
2025-07-01 12:21:46 +0800

    fix issue: can not enable local-cluster when globalhub installed (#1769)

--- test/integration/operator/webhook/admission_handler_test.go
+++ test/integration/operator/webhook/admission_handler_test.go
@@ -9,30 +9,47 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	addonv1 "github.com/stolostron/klusterlet-addon-controller/pkg/apis/agent/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/stolostron/multicluster-global-hub/pkg/constants"
-	"github.com/stolostron/multicluster-global-hub/pkg/utils"
 )
+
+var testmanagedcluster = &clusterv1.ManagedCluster{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "mc1",
+		Labels: map[string]string{
+			// global hub featuregate for klusterlet hosted, this label for addon hosted
+			constants.GHDeployModeLabelKey: constants.GHDeployModeHosted,
+		},
+		Annotations: map[string]string{},
+	},
+}
+
+var klusterletConfig = &addonv1.KlusterletAddonConfig{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "mc1",
+		Namespace: "mc1",
+	},
+	Spec: addonv1.KlusterletAddonConfigSpec{
+		ApplicationManagerConfig: addonv1.KlusterletAddonAgentConfigSpec{
+			Enabled: true,
+		},
+		PolicyController: addonv1.KlusterletAddonAgentConfigSpec{
+			Enabled: true,
+		},
+		CertPolicyControllerConfig: addonv1.KlusterletAddonAgentConfigSpec{
+			Enabled: true,
+		},
+	},
+}
 
 var _ = Describe("Multicluster hub webhook", func() {
 	Context("Test managedclusters are handled by the global hub manager webhook", Ordered, func() {
 		It("managedcluster should be added the hosted annotations", func() {
-			testmanagedcluster := &clusterv1.ManagedCluster{
-				ObjectMeta: metav1.ObjectMeta{
-					GenerateName: "test-mc-",
-					Namespace:    utils.GetDefaultNamespace(),
-					Labels: map[string]string{
-						// global hub featuregate for klusterlet hosted, this label for addon hosted
-						constants.GHDeployModeLabelKey: constants.GHDeployModeHosted,
-					},
-					Annotations: map[string]string{},
-				},
-			}
-
 			Eventually(func() bool {
 				if err := c.Create(ctx, testmanagedcluster, &client.CreateOptions{}); err != nil {
 					return false
@@ -54,23 +71,11 @@ var _ = Describe("Multicluster hub webhook", func() {
 
 	Context("Test klusterletaddonconfig are handled by the global hub manager webhook", Ordered, func() {
 		It("klusterletaddonconfig should be added the hosted annotations", func() {
-			klusterletConfig := &addonv1.KlusterletAddonConfig{
+			Expect(c.Create(ctx, &corev1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test",
-					Namespace: "default",
+					Name: "mc1",
 				},
-				Spec: addonv1.KlusterletAddonConfigSpec{
-					ApplicationManagerConfig: addonv1.KlusterletAddonAgentConfigSpec{
-						Enabled: true,
-					},
-					PolicyController: addonv1.KlusterletAddonAgentConfigSpec{
-						Enabled: true,
-					},
-					CertPolicyControllerConfig: addonv1.KlusterletAddonAgentConfigSpec{
-						Enabled: true,
-					},
-				},
-			}
+			})).To(Succeed())
 
 			Eventually(func() bool {
 				if err := c.Create(ctx, klusterletConfig, &client.CreateOptions{}); err != nil {
