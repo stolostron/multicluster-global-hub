29178d7d76106795951594ed106a7617e8b096a0
Hui Chen <huichen@redhat.com>
2025-08-14 14:35:37 +0800

    pass the branch ref to QE test tools (#1882)
    Signed-off-by: hchenxa <huichen@redhat.com>

--- test/script/e2e_run_qe.sh
+++ test/script/e2e_run_qe.sh
@@ -12,8 +12,15 @@ CONFIG_DIR=$CURRENT_DIR/config
 GH_KUBECONFIG="${CONFIG_DIR}/global-hub"
 MH1_KUBECONFIG="${CONFIG_DIR}/hub1"
 
-docker pull quay.io/hchenxa/acmqe-hoh-e2e:v1.5
-docker run --name qe-test -d quay.io/hchenxa/acmqe-hoh-e2e:v1.5  tail -f /dev/null
+if [[ $PULL_BASE_REF =~ 'release-' ]]; then
+  branchVersion=$(echo -n $branchName | awk -F '-' '{print $2}')
+  imageTag="v$branchVersion"
+else
+  imageTag="latest"
+fi
+
+docker pull quay.io/hchenxa/acmqe-hoh-e2e:$imageTag
+docker run --name qe-test -d quay.io/hchenxa/acmqe-hoh-e2e:$imageTag  tail -f /dev/null
 docker cp qe-test:/e2e.test ./
 
 SERVICE_TYPE=NODE_PORT KUBECONFIG=$GH_KUBECONFIG SPOKE_KUBECONFIG=$MH1_KUBECONFIG ./e2e.test --ginkgo.fail-fast --ginkgo.vv --ginkgo.label-filter='e2e && !migration'
