a4e5e32b1d4068ce101ce4c87779e4b697b327f8
Meng Yan <myan@redhat.com>
2025-08-21 14:43:51 +0800

    rollback update
    Signed-off-by: Meng Yan <myan@redhat.com>

--- operator/pkg/controllers/agent/local_agent_controller.go
+++ operator/pkg/controllers/agent/local_agent_controller.go
@@ -281,7 +281,7 @@ func GenerateLocalAgentCredential(ctx context.Context, c client.Client, namespac
 		return err
 	}
 	log.Debugf("kafkaConnection: %v", *kafkaConnection)
-	kafkaConfigYaml, err := kafkaConnection.ToYAML(true)
+	kafkaConfigYaml, err := kafkaConnection.YamlMarshal(true)
 	if err != nil {
 		return fmt.Errorf("failed to marshalling the kafka config yaml: %w", err)
 	}
