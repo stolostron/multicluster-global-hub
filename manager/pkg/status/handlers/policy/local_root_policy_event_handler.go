5f9adc3c5d744f0aa09065d5d6b7ddd41a002005
Meng Yan <myan@redhat.com>
2025-10-16 17:54:50 +0800

    update
    Signed-off-by: Meng Yan <myan@redhat.com>

--- manager/pkg/status/handlers/policy/local_root_policy_event_handler.go
+++ manager/pkg/status/handlers/policy/local_root_policy_event_handler.go
@@ -140,7 +140,8 @@ func (h *localRootPolicyEventHandler) handleEvent(ctx context.Context, evt *clou
 			continue
 		}
 		// Create a unique key based on the ON CONFLICT columns
-		key := fmt.Sprintf("%s-%d-%s", localEvent.EventName, localEvent.Count, localEvent.CreatedAt.Format("2006-01-02T15:04:05.999999999Z07:00"))
+		key := fmt.Sprintf("%s-%d-%s", localEvent.EventName, localEvent.Count,
+			localEvent.CreatedAt.Format("2006-01-02T15:04:05.999999999Z07:00"))
 		// Keep the last occurrence if there are duplicates
 		eventMap[key] = localEvent
 	}
