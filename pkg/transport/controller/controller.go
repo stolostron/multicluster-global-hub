7a0aa2091ff34a8b2ac71471f37ea5d568271c2b
Meng Yan <myan@redhat.com>
2025-11-12 16:06:54 +0800

    refactor: remove redundant owner identity assignment
    Remove unnecessary variable assignment where ownerIdentity was
    redundantly set to kafkaConn.ClusterID in both branches of the
    conditional. Simplify by directly using kafkaConn.ClusterID in
    SetKafkaOwnerIdentity call.
    
    Signed-off-by: myan <morvencao@gmail.com>
    Signed-off-by: Meng Yan <myan@redhat.com>

--- pkg/transport/controller/controller.go
+++ pkg/transport/controller/controller.go
@@ -283,10 +283,6 @@ func (c *TransportCtrl) ReconcileKafkaCredential(ctx context.Context, secret *co
 	}
 	c.transportConfig.KafkaCredential = kafkaConn
 	// owner identity is the cluster identity to record the offset of specific cluster
-	ownerIdentity := kafkaConn.ClusterID
-	if ownerIdentity == "" {
-		ownerIdentity = kafkaConn.ClusterID
-	}
 	config.SetKafkaOwnerIdentity(kafkaConn.ClusterID)
 	return true, nil
 }
