1322bd2b1f783609f226274804f6a0b6618426e1
ab-ghosh <abghosh@redhat.com>
2026-09-14 18:01:22 +0530

    test: cover migration consumer lifecycle
    Signed-off-by: ab-ghosh <abghosh@redhat.com>

--- pkg/transport/consumer/generic_consumer_test.go
+++ pkg/transport/consumer/generic_consumer_test.go
@@ -1,12 +1,16 @@
 package consumer
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
 	"testing"
 	"time"
 
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	ceprotocol "github.com/cloudevents/sdk-go/v2/protocol"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm/clause"
@@ -15,6 +19,7 @@ import (
 	"github.com/stolostron/multicluster-global-hub/pkg/database"
 	"github.com/stolostron/multicluster-global-hub/pkg/database/models"
 	"github.com/stolostron/multicluster-global-hub/pkg/transport"
+	"github.com/stolostron/multicluster-global-hub/pkg/transport/producer"
 	"github.com/stolostron/multicluster-global-hub/test/integration/utils/testpostgres"
 )
 
@@ -58,6 +63,132 @@ func TestNewGenericConsumerSeparatesMigrationTopic(t *testing.T) {
 	assert.NotNil(t, transportConfig.Extends["gh-spec"])
 	assert.NotNil(t, transportConfig.Extends["gh-migration"])
 	assert.NotEqual(t, transportConfig.Extends["gh-spec"], transportConfig.Extends["gh-migration"])
+}
+
+func TestGenericConsumerReceivesMigrationEventsSeparately(t *testing.T) {
+	transportConfig := &transport.TransportInternalConfig{
+		TransportType: string(transport.Chan),
+		KafkaCredential: &transport.KafkaConfig{
+			SpecTopic:      "gh-spec",
+			MigrationTopic: "gh-migration",
+		},
+	}
+
+	consumer, err := NewGenericConsumer(transportConfig, []string{"gh-spec", "gh-migration"})
+	assert.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	done := make(chan error, 1)
+	go func() { done <- consumer.Start(ctx) }()
+	t.Cleanup(func() {
+		cancel()
+		select {
+		case err := <-done:
+			assert.NoError(t, err)
+		case <-time.After(time.Second):
+			t.Error("consumer did not stop after context cancellation")
+		}
+	})
+
+	producer, err := producer.NewGenericProducer(transportConfig, "gh-migration", nil)
+	assert.NoError(t, err)
+	event := cloudevents.NewEvent()
+	event.SetType("migration.event")
+	event.SetSource("test")
+	assert.NoError(t, producer.SendEvent(ctx, event))
+
+	select {
+	case received := <-consumer.EventChan():
+		assert.Equal(t, "migration.event", received.Type())
+	case <-time.After(time.Second):
+		t.Fatal("did not receive migration event")
+	}
+}
+
+func TestGenericConsumerStopsMigrationRetryAfterCancellation(t *testing.T) {
+	receiver := &failingCloudEventsClient{started: make(chan struct{}, 1)}
+	consumer := &GenericConsumer{}
+	ctx, cancel := context.WithCancel(context.Background())
+	done := make(chan struct{})
+	go func() {
+		consumer.receiveMigrationEvents(ctx, receiver)
+		close(done)
+	}()
+
+	select {
+	case <-receiver.started:
+		cancel()
+	case <-time.After(time.Second):
+		t.Fatal("migration receiver was not started")
+	}
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+		t.Fatal("migration receiver did not stop after context cancellation")
+	}
+}
+
+func TestGenericConsumerReconnectsMigrationReceiver(t *testing.T) {
+	transportConfig := &transport.TransportInternalConfig{
+		TransportType: string(transport.Chan),
+		KafkaCredential: &transport.KafkaConfig{
+			SpecTopic:       "gh-spec",
+			MigrationTopic:  "gh-migration",
+			ConsumerGroupID: "test-consumer",
+		},
+	}
+
+	consumer, err := NewGenericConsumer(transportConfig, []string{"gh-spec", "gh-migration"})
+	assert.NoError(t, err)
+	consumerStopped := make(chan struct{})
+	migrationStopped := make(chan struct{})
+	consumer.consumerCancel = func() { close(consumerStopped) }
+	consumer.migrationCancel = func() { close(migrationStopped) }
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+	assert.NoError(t, consumer.Reconnect(ctx, transportConfig, []string{"gh-spec", "gh-migration"}))
+	select {
+	case <-consumerStopped:
+	case <-time.After(time.Second):
+		t.Fatal("previous consumer was not cancelled")
+	}
+	select {
+	case <-migrationStopped:
+	case <-time.After(time.Second):
+		t.Fatal("previous migration receiver was not cancelled")
+	}
+
+	producer, err := producer.NewGenericProducer(transportConfig, "gh-migration", nil)
+	assert.NoError(t, err)
+	event := cloudevents.NewEvent()
+	event.SetType("migration.reconnect")
+	event.SetSource("test")
+	assert.NoError(t, producer.SendEvent(ctx, event))
+
+	select {
+	case received := <-consumer.EventChan():
+		assert.Equal(t, "migration.reconnect", received.Type())
+	case <-time.After(time.Second):
+		t.Fatal("did not receive migration event after reconnect")
+	}
+}
+
+type failingCloudEventsClient struct {
+	started chan struct{}
+}
+
+func (c *failingCloudEventsClient) Send(context.Context, cloudevents.Event) ceprotocol.Result {
+	return ceprotocol.ResultACK
+}
+
+func (c *failingCloudEventsClient) Request(context.Context, cloudevents.Event) (*cloudevents.Event, ceprotocol.Result) {
+	return nil, ceprotocol.ResultACK
+}
+
+func (c *failingCloudEventsClient) StartReceiver(context.Context, interface{}) error {
+	c.started <- struct{}{}
+	return errors.New("topic authorization failed")
 }
 
 func TestGetInitOffset(t *testing.T) {
